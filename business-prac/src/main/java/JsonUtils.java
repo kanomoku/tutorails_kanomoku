3a4f119c78c5f98205ccf39fa712ca202afa2a00
kanomoku <1049505015@qq.com>
2022-04-26 23:14:05 +0800

    feat: 追加JsonUtils类
    JSON转换方法的学习

---
+++ business-prac/src/main/java/JsonUtils.java
@@ -0,0 +1,28 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.json.JsonSanitizer;
+
+public class JsonUtils {
+
+    public static <T> T fromJson(String json, Class<T> clazz) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        T object = null;
+        try {
+            object = objectMapper.readValue(JsonSanitizer.sanitize(json), clazz);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
+    public static <T> String toJson(T object) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonStr = null;
+        try {
+            jsonStr = objectMapper.writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonStr;
+    }
+}
