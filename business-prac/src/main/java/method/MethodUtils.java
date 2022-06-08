ac350dd5211af8e1536058ef195ad5b3175445a0
kanomoku <1049505015@qq.com>
2022-06-08 23:12:46 +0800

    feat: 追加场景模块、一个对象被更新后、获取更新了的属性和属性的内容

--- business-prac/src/main/java/method/MethodUtils.java
+++ business-prac/src/main/java/method/MethodUtils.java
@@ -1,13 +1,67 @@
 package method;
 
-import org.apache.commons.collections.CollectionUtils;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ObjectUtils;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class MethodUtils {
+    public static List<Map<String, String>> getChange(Object originObject, Object newObject)
+        throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+        // 存在为null比价就没意义了
+        if (ObjectUtils.anyNull(originObject, newObject)) {
+            return null;
+        }
+        // 不同类型无法比较
+        if (ObjectUtils.notEqual(originObject.getClass(), newObject.getClass())) {
+            return null;
+        }
+        // 内容都相等所以没必要比较
+        if (Objects.deepEquals(originObject, newObject)) {
+            return Lists.newArrayList();
+        }
+        Map<String, String> originFieldValueMap = Maps.newHashMap();
+        Map<String, String> newFieldValueMap = Maps.newHashMap();
+        Field[] declaredFields = originObject.getClass().getDeclaredFields();
+        for (Field field : declaredFields) {
+            // 检查Field是否为合成字段、如果该字段是合成字段则不进行比较
+            if (field.isSynthetic()) {
+                continue;
+            }
+
+            // 实现方式1
+            // field.setAccessible(true);
+            // Object o = field.get(originObject);
+            // Object n = field.get(originObject);
+
+            // 实现方式2
+            PropertyDescriptor propertyDescriptor = new PropertyDescriptor(field.getName(), originObject.getClass());
+            Method readMethod = propertyDescriptor.getReadMethod();
+            Object originValue = readMethod.invoke(originObject);
+            Object newValue = readMethod.invoke(newObject);
+            if (Objects.deepEquals(originValue, newValue)) {
+                // 值不相等的话记录属性和值
+                originFieldValueMap.put(field.getName(), Objects.isNull(originValue) ? null : originValue.toString());
+                newFieldValueMap.put(field.getName(), Objects.isNull(newValue) ? null : newValue.toString());
+            }
+        }
+        List<Map<String, String>> changes = Lists.newArrayList();
+        changes.add(originFieldValueMap);
+        changes.add(newFieldValueMap);
+        return changes;
+    }
+
     public List<Integer> processTabIds(List<Integer> newTagIds, List<Integer> addTagIds, List<Integer> deleteTagIds,
         List<Integer> existingTagIds) {
         List<Integer> tagIds;
