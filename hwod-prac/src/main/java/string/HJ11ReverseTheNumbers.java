dbf7f44d4bddc8bb1afb9e874671bfa14c54fd1b
zhangziwa <1049505015@qq.com>
2023-02-12 14:38:38 +0800

    【算法题】HJ11 数字颠倒
    Took 2 minutes

--- hwod-prac/src/main/java/string/HJ11ReverseTheNumbers.java
+++ hwod-prac/src/main/java/string/HJ11ReverseTheNumbers.java
@@ -6,24 +6,31 @@ import java.io.InputStreamReader;
 
 import static java.lang.System.in;
 
+/**
+ * HJ11 数字颠倒
+ */
 public class HJ11ReverseTheNumbers {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(in));
         String s = bf.readLine();
         bf.close();
 
-        String s1 = getString(s);
+        String s1 = getReverseString2(s);
         System.out.println(s1);
 
     }
 
-    private static String getString(String s) {
+    private static String getReverseString1(String s) {
         char[] chars = s.toCharArray();
         StringBuilder sb = new StringBuilder();
         for (int i = chars.length - 1; i >= 0; i--) {
             sb.append(chars[i]);
         }
-        String s1 = sb.toString();
-        return s1;
+        return sb.toString();
+    }
+
+    private static String getReverseString2(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString();
     }
 }
