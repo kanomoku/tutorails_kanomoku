34d7ab6072b8bcff6ab80819d405f4ff30a6fcf3
zhangziwa <1049505015@qq.com>
2023-02-15 23:02:09 +0800

    [算法题]leetcode 674. 无序整数数组的最长且连续递增的子序列
    Took 29 minutes

--- hwod-prac/src/main/java/doublepoint/leetcode674LongestIncreasingContinuousSubsequence.java
+++ hwod-prac/src/main/java/doublepoint/leetcode674LongestIncreasingContinuousSubsequence.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import static java.lang.System.in;
 
 /**
- * 674. 最长连续递增序列
+ * leetcode 674. 无序整数数组的最长且连续递增的子序列
  */
 public class leetcode674LongestIncreasingContinuousSubsequence {
     public static void main(String[] args) throws IOException {
@@ -18,14 +18,21 @@ public class leetcode674LongestIncreasingContinuousSubsequence {
 
         int lengthOfLCIS = findLengthOfLCIS(ints);
         System.out.println(lengthOfLCIS);
+
+        int[] lengthOfLCIS1 = findLengthOfLCIS1(ints);
+        System.out.println(Arrays.toString(lengthOfLCIS1));
     }
 
+    /**
+     * leetcode 674. 无序整数数组的最长且连续递增的子序列的长度
+     */
     public static int findLengthOfLCIS(int[] nums) {
         if (nums.length < 1) {
             return 0;
         }
 
         int max = 1;
+
         int start = 0;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] < nums[i]) {
@@ -35,6 +42,32 @@ public class leetcode674LongestIncreasingContinuousSubsequence {
                 start = i;
             }
         }
+
         return max;
+    }
+
+    /**
+     * leetcode 674. 无序整数数组的最长且连续递增的子序列
+     */
+    public static int[] findLengthOfLCIS1(int[] nums) {
+        if (nums.length < 2) {
+            return nums;
+        }
+
+        int max = 1;
+        int[] temp = {nums[0]};
+
+        int start = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                if (i - start + 1 > max) {
+                    max = i - start + 1;
+                    temp = Arrays.copyOfRange(nums, start, i + 1);
+                }
+            } else {
+                start = i;
+            }
+        }
+        return temp;
     }
 }
