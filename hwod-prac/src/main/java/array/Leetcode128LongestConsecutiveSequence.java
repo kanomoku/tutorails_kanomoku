06285636d46c25f4941c4f72ec972ab7e462e4b5
zhangziwa <1049505015@qq.com>
2023-02-16 19:18:57 +0800

    [算法题]leetcode128 无序整数数组,找出数字连续的最长序列的长度（不要求序列元素在原数组中连续）
    Took 5 minutes

--- hwod-prac/src/main/java/array/Leetcode128LongestConsecutiveSequence.java
+++ hwod-prac/src/main/java/array/Leetcode128LongestConsecutiveSequence.java
@@ -7,6 +7,9 @@ import java.util.Arrays;
  */
 public class Leetcode128LongestConsecutiveSequence {
 
+    /**
+     * leetcode128 无序整数数组,找出数字连续的最长序列的长度（不要求序列元素在原数组中连续）
+     */
     public int longestConsecutive(int[] nums) {
         if (nums.length == 0) {
             return 0;
@@ -17,23 +20,28 @@ public class Leetcode128LongestConsecutiveSequence {
 
         Arrays.sort(nums);
 
+        // 序列起码自己所以初始值为1而非0
         int max = 1;
-        int tempMax = 1;
+        int temp = 1;
+
         for (int i = 1; i < nums.length; i++) {
             if ((nums[i - 1]) == nums[i]) {
-                // 数字连续即表示严格递增
+                // 数字连续即要求严格递增，当前处理是针对如下场景
+                // [100,4,200,1,3,3,3,3,2] → [1,2,3,4] → 4
                 continue;
             }
+
             if ((nums[i - 1] + 1) == nums[i]) {
                 // 符合连续条件
-                tempMax++;
+                temp++;
             } else {
-                max = Math.max(tempMax, max);
-                tempMax = 1;
+                max = Math.max(temp, max); // 动态收集最大值
+                temp = 1; // 开启新一轮
             }
         }
 
-        max = Math.max(tempMax, max);
+        // 此处针对最后一位走的是 符合连续条件 然后循环完毕 这种场景
+        max = Math.max(temp, max);
 
         return max;
     }
