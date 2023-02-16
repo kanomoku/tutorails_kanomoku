8b467918a52b8793aa6c10bcca77039d1f81d013
zhangziwa <1049505015@qq.com>
2023-02-16 20:08:38 +0800

    [算法题]数组拼接
    Took 2 minutes

--- hwod-prac/src/main/java/array/ArraySplicing.java
+++ hwod-prac/src/main/java/array/ArraySplicing.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+// 数组拼接
 //  3
 //  2,5,6,7,9,5,7
 //  1,7,4,3,4
@@ -30,42 +31,48 @@ public class ArraySplicing {
         }
         bf.close();
 
-        // 收集新数组元素
+        List<Integer> res = getIntegers(list, len);
+
+        String collect = res.stream().map(String::valueOf).collect(Collectors.joining(","));
+        System.out.println(collect);
+    }
+
+    /**
+     * 数组按照固定长度拼接
+     *
+     * @param list      数组
+     * @param targetLen 长度
+     */
+    private static List<Integer> getIntegers(List<List<Integer>> list, int targetLen) {
         List<Integer> res = new ArrayList<>();
 
-        // 每一轮截取的初始下标
-        int index = 0;
-        // 动态标记→初始条件→判断是否所有数据均截取完毕
-        boolean allFinished = false;
-        // 临时变量纯为了提升性能→取名我篮子
-        List<Integer> temp;
+
+        int index = 0; // 每一轮截取的初始下标
+        boolean allFinished = false; // 动态标记→初始条件→判断是否所有数据均截取完毕
+        List<Integer> temp; // 临时变量纯为了提升性能→取名为篮子
 
         while (!allFinished) {
             // 所有数组均遍历一次
             for (int i = 0; i < list.size(); i++) {
 
-                temp = new ArrayList<>(); // 每次循环清空篮子
                 allFinished = true; // 动态标记→终止条件→每一轮都默认为所有数组处理完毕
+                temp = new ArrayList<>(); // 每次循环清空篮子
 
-                List<Integer> integers = list.get(i);
-                if (index + len <= integers.size()) { // 右边界在数组范围内
-                    temp = integers.subList(index, index + len);
+                List<Integer> subList = list.get(i);
+                if (index + targetLen <= subList.size()) { // 右边界在范围内
+                    temp = subList.subList(index, index + targetLen);
                     allFinished = false; // 动态标记→动态打flag→存在正常截取则表示还有未处理完毕的数组
-                } else if (index < integers.size()) { // 右边界不在数组范围内，左边界在数组范围内
-                    temp = integers.subList(index, integers.size());
+                } else if (index < subList.size()) { // 右边界不在范围内，起始索引还在数组范围内
+                    temp = subList.subList(index, subList.size());
                 } else {
                     // 左边界不在数组范围内
                 }
 
-                // 收集元素
-                res.addAll(temp);
+                res.addAll(temp); // 收集元素
             }
 
-            // 初始下标右移
-            index = index + len;
+            index = index + targetLen; // 初始下标右移
         }
-
-        String collect = res.stream().map(String::valueOf).collect(Collectors.joining(","));
-        System.out.println(collect);
+        return res;
     }
 }
