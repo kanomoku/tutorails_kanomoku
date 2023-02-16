5bfaacb0d6217727df11fed17fec725964b5f76c
zhangziwa <1049505015@qq.com>
2023-02-16 19:36:37 +0800

    [算法题]HJ41 称砝码
    Took 10 minutes

--- hwod-prac/src/main/java/dfs/HJ41WeightIndication.java
+++ hwod-prac/src/main/java/dfs/HJ41WeightIndication.java
@@ -12,7 +12,7 @@ import static java.lang.System.in;
 /**
  * HJ41 称砝码
  */
-public class WeightIndication {
+public class HJ41WeightIndication {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(in));
         // n种砝码
@@ -23,26 +23,38 @@ public class WeightIndication {
         Integer[] nums = Arrays.stream(bf.readLine().split(" ")).limit(n).map(Integer::parseInt).toArray(Integer[]::new);
         bf.close();
 
+        HashSet<Integer> allCase = getWeights(n, weights, nums);
+
+        System.out.println(allCase.size());
+    }
+
+    /**
+     * @param kinds     n种砝码
+     * @param weightPer 每种砝码重量
+     * @param countPer  每种几个
+     */
+    private static HashSet<Integer> getWeights(int kinds, Integer[] weightPer, Integer[] countPer) {
         // 所有砝码重量组合情况set
         HashSet<Integer> allCase = new HashSet<>();
 
         // 由示例可知，0重量也属于一种情况
         allCase.add(0);
 
-        for (int i = 0; i < n; i++) { //每一种砝码
+        for (int i = 0; i < kinds; i++) { //每一种砝码
+            HashSet<Integer> curKindWeights = new HashSet<>();
 
-            HashSet<Integer> current = new HashSet<>();
+            for (int k = 1; k <= countPer[i]; k++) { // 每种砝码遍历每个
+                int stepWeight = weightPer[i] * k; // 砝码个数叠加枚举
+                curKindWeights.add(stepWeight);
 
-            for (int k = 1; k <= nums[i]; k++) { // 每种砝码遍历每个
-                int stepPlusWeight = weights[i] * k; // 砝码个数叠加枚举
                 for (Integer weight : allCase) {
-                    current.add(stepPlusWeight);
-                    current.add(weight + stepPlusWeight);
+                    curKindWeights.add(weight + stepWeight);
                 }
             }
 
-            allCase.addAll(current);
+            allCase.addAll(curKindWeights);
         }
-        System.out.println(allCase.size());
+
+        return allCase;
     }
 }
