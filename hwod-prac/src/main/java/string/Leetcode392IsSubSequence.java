22c42da2227ea291743f78b08d5459c8e3d21869
zhangziwa <1049505015@qq.com>
2023-02-16 12:42:35 +0800

    [算法题]leetcode392 判断字符串 s 是否为 t 的子序列
    Took 4 minutes

--- hwod-prac/src/main/java/string/Leetcode392IsSubSequence.java
+++ hwod-prac/src/main/java/string/Leetcode392IsSubSequence.java
@@ -28,6 +28,27 @@ public class Leetcode392IsSubSequence {
         System.out.println(res);
     }
 
+    private static boolean isSubsequence2(String s, String t) {
+        if (s.length() == 0) {
+            return true;
+        }
+        if (t.length() == 0) {
+            return false;
+        }
+
+        int index = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (s.charAt(index) == t.charAt(i)) {
+                index++;
+                if (index >= s.length()) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
     /**
      * 判断字符串 s 是否为 t 的子序列
      */
@@ -44,17 +65,11 @@ public class Leetcode392IsSubSequence {
 
         int index = 0;
         for (int i = 0; i < mother.length; i++) {
-            while (index < son.length) { // 还在有效范围内
-                if (mother[i] == son[index]) {
-                    if (index >= son.length - 1) { // 最后一个字符是相等的
-                        return true;
-                    }
-                    index++; // 当前字符处理完指针挪到下个位置，为后续做准备
-                    break; // 如果找到，都进行下一个字符的处理（这里很关键，不然判断完的还在判断）
-                } else {
-                    // 不相等的话，短字符串index不动，长字符串挪动到下一位
-                    break;
+            if (mother[i] == son[index]) {
+                if (index >= son.length - 1) { // 最后一个字符是相等的
+                    return true;
                 }
+                index++; // 当前字符处理完指针挪到下个位置，为后续做准备
             }
         }
 
