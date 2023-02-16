168c1a53003891bca871e748cf404a341fe61fad
zhangziwa <1049505015@qq.com>
2023-02-16 13:33:11 +0800

    [算法题]华为od题 56题 考勤问题
    Took 2 minutes

--- hwod-prac/src/main/java/logic/AttendanceInformation.java
+++ hwod-prac/src/main/java/logic/AttendanceInformation.java
@@ -44,39 +44,14 @@ public class AttendanceInformation {
             }
 
             // 2.没有连续的迟到/早退
-            List<String> absent = Arrays.asList("late", "leaveearly");
-            boolean isBad = false;
-            for (int i = 1; i < person.size(); i++) {
-                String current = person.get(i);
-                String last = person.get(i - 1);
-                if (absent.contains(current) && absent.contains(last)) {
-                    isBad = true;
-                    break;
-                }
-            }
+            boolean isBad = isBad(person);
             if (isBad) {
                 res.add(false);
                 continue;
             }
 
             // 3.任意连续7次考勤 缺勤/迟到/早退 不超过3次
-            int[] arr = new int[person.size()];
-            for (int i = 0; i < person.size(); i++) {
-                arr[i] = "present".equals(person.get(i)) ? 0 : 1;
-            }
-            boolean isBad2 = false;
-            if (arr.length <= 7) {
-                isBad2 = Arrays.stream(arr).sum() > 3;
-            } else {
-                for (int i = 0; i <= arr.length - 7; i++) {
-                    int[] ints = Arrays.copyOfRange(arr, i, i + 7); // 任意7天
-                    int sum = Arrays.stream(ints).sum();
-                    if (sum > 3) {
-                        isBad2 = true;
-                        break;
-                    }
-                }
-            }
+            boolean isBad2 = isBad2(person);
             if (isBad2) {
                 res.add(false);
                 continue;
@@ -86,5 +61,53 @@ public class AttendanceInformation {
 
         String collect = res.stream().map(String::valueOf).collect(Collectors.joining(","));
         System.out.println(collect);
+    }
+
+    /**
+     * 判断是否任意连续7次考勤 缺勤/迟到/早退 超过3次
+     */
+    private static boolean isBad2(List<String> attendances) {
+        if (attendances.size() == 1) {
+            return false;
+        }
+
+        int[] arr = new int[attendances.size()];
+        for (int i = 0; i < attendances.size(); i++) {
+            arr[i] = "present".equals(attendances.get(i)) ? 0 : 1;
+        }
+
+        if (arr.length <= 7) {
+            return Arrays.stream(arr).sum() > 3;
+        } else {
+            for (int i = 0; i <= arr.length - 7; i++) {
+                int[] ints = Arrays.copyOfRange(arr, i, i + 7); // 任意7天
+                int sum = Arrays.stream(ints).sum();
+                if (sum > 3) {
+                    // 存在有问题数据就直接终止判断并返回
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断是否连续的迟到/早退
+     */
+    private static boolean isBad(List<String> attendances) {
+        if (attendances.size() <= 1) {
+            return false;
+        }
+
+        List<String> absent = Arrays.asList("late", "leaveearly");
+        for (int i = 1; i < attendances.size(); i++) {
+            String current = attendances.get(i);
+            String previous = attendances.get(i - 1);
+            if (absent.contains(current) && absent.contains(previous)) {
+                // 存在有问题数据就直接终止判断并返回
+                return true;
+            }
+        }
+        return false;
     }
 }
