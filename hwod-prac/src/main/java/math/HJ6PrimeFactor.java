612f00f3a090dba329d6516414246d123bde1c29
zhangziwa <1049505015@qq.com>
2023-02-17 12:23:59 +0800

    [算法题]HJ6 质数因子
    Took 1 minute

--- hwod-prac/src/main/java/math/HJ6PrimeFactor.java
+++ hwod-prac/src/main/java/math/HJ6PrimeFactor.java
@@ -9,12 +9,26 @@ import java.util.stream.Collectors;
 
 import static java.lang.System.in;
 
+/**
+ * HJ6 质数因子
+ */
 public class HJ6PrimeFactor {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(in));
         Long num = Long.parseLong(bf.readLine());
         bf.close();
 
+        List<Long> res = getPrimes(num);
+
+        // 输出素数因子
+        String str = res.stream().map(String::valueOf).collect(Collectors.joining(" "));
+        System.out.println(str);
+    }
+
+    /**
+     * 收集质数因子
+     */
+    private static List<Long> getPrimes(Long num) {
         List<Long> res = new ArrayList<>();
 
         // Math.sqrt(num)两数相乘最大的也就是平方，再往后就是重复值了
@@ -25,18 +39,11 @@ public class HJ6PrimeFactor {
                 num = num / i;
             }
         }
-        // num==1说明全部整除开了，num!=1说明余下的就是素数了
+
+        // num==1说明全部整除开了，num!=1说明余下本身为素数
         if (num != 1) {
             res.add(num);
         }
-
-        // 没有素数因子，输出输入的数
-        if (res.size() == 0) {
-            System.out.println(num);
-        } else {
-            // 输出素数因子
-            String str = res.stream().map(String::valueOf).collect(Collectors.joining(" "));
-            System.out.println(str);
-        }
+        return res;
     }
 }
