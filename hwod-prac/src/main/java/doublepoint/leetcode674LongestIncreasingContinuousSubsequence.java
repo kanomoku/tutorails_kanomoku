6e7d0fa9a76b5b384ea0874ebc2bdcb41de2ecfd
zhangziwa <1049505015@qq.com>
2023-02-17 19:59:29 +0800

    【算法题】leetcode 674. 无序整数数组的最长且连续递增的子序列
    Took 12 minutes

--- hwod-prac/src/main/java/doublepoint/leetcode674LongestIncreasingContinuousSubsequence.java
+++ hwod-prac/src/main/java/doublepoint/leetcode674LongestIncreasingContinuousSubsequence.java
@@ -16,7 +16,7 @@ public class leetcode674LongestIncreasingContinuousSubsequence {
         int[] ints = Arrays.stream(bf.readLine().split(",")).mapToInt(Integer::parseInt).toArray();
         bf.close();
 
-        int lengthOfLCIS = findLengthOfLCIS(ints);
+        int lengthOfLCIS = findLengthOfLCIS2(ints);
         System.out.println(lengthOfLCIS);
 
         int[] lengthOfLCIS1 = findLengthOfLCIS1(ints);
@@ -27,8 +27,12 @@ public class leetcode674LongestIncreasingContinuousSubsequence {
      * leetcode 674. 无序整数数组的最长且连续递增的子序列的长度
      */
     public static int findLengthOfLCIS(int[] nums) {
-        if (nums.length < 1) {
+        if (nums.length == 0) {
             return 0;
+        }
+
+        if (nums.length == 1) {
+            return 1;
         }
 
         int max = 1;
@@ -47,27 +51,58 @@ public class leetcode674LongestIncreasingContinuousSubsequence {
     }
 
     /**
+     * leetcode 674. 无序整数数组的最长且连续递增的子序列的长度
+     */
+    public static int findLengthOfLCIS2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        if (nums.length == 1) {
+            return 1;
+        }
+
+        int max = 1;
+
+        int tempMax = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                tempMax++;
+            } else {
+                max = Math.max(tempMax, max);
+                tempMax = 1;
+            }
+        }
+        max = Math.max(tempMax, max);
+
+        return max;
+    }
+
+    /**
      * leetcode 674. 无序整数数组的最长且连续递增的子序列
      */
     public static int[] findLengthOfLCIS1(int[] nums) {
-        if (nums.length < 2) {
+        if (nums.length < 2) { // 2个一下不涉及排序
             return nums;
         }
 
         int max = 1;
-        int[] temp = {nums[0]};
+        int[] temp = {nums[0]};  // 下边从1开始和前面的值比较,起码有前面这个值,所以收集第一个元素应对[2,2,2,2,2]这种场景
 
         int start = 0;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] < nums[i]) {
                 if (i - start + 1 > max) {
                     max = i - start + 1;
+                    // 方法1：直接截取数组
                     temp = Arrays.copyOfRange(nums, start, i + 1);
+                    // 方法2：记录开始结束坐标最后再截取
                 }
             } else {
                 start = i;
             }
         }
+
         return temp;
     }
 }
