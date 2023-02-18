f58eccaee0b149c4e1c63147ec6ee82e8b3c543f
zhangziwa <1049505015@qq.com>
2023-02-18 20:46:32 +0800

    【算法题】LeetCode77. 组合
    Took 11 minutes

---
+++ hwod-prac/src/main/java/dfs/Leetcode77Combinations.java
@@ -0,0 +1,55 @@
+package dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.in;
+
+/**
+ * LeetCode77. 组合
+ * 范围 [1, n] 中所有可能的 k 个数的组合
+ */
+public class Leetcode77Combinations {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(in));
+        bf.close();
+
+        int n = 5; // n个数，1,2,3,4,5
+        int k = 3; // 任意三个的组合
+        List<List<Integer>> res = combine(n, k);
+        System.out.println(res);
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        // 范围 [1, n] 中所有可能的 k 个数的组合
+        // k<=0时没意义;
+        // n<k时没意义,一共才n个想取k个不合理
+        if (k <= 0 || n < k) {
+            return res;
+        }
+
+        Deque<Integer> stack = new LinkedList<>();
+        dfs(1, n, k, stack, res);
+
+        return res;
+    }
+
+    public static void dfs(int start, int milestone, int limit, Deque<Integer> stack, List<List<Integer>> res) {
+        if (stack.size() == limit) { // 一共收集limit层,已经收集到limit层时直接收工大退
+            res.add(new ArrayList<>(stack));
+            System.out.println(new ArrayList<>(stack));
+            return;
+        }
+
+        for (int i = start; i <= milestone; i++) {
+            stack.push(i);
+            dfs(i + 1, milestone, limit, stack, res);
+            stack.pop();
+        }
+    }
+}
