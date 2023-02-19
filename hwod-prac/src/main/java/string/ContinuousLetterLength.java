18f568a0a5d7b6a08e1ab83cbe77ff9f8d04fb46
zhangziwa <1049505015@qq.com>
2023-02-19 10:36:11 +0800

    【算法题】连续字母长度
    Took 7 minutes

--- hwod-prac/src/main/java/string/ContinuousLetterLength.java
+++ hwod-prac/src/main/java/string/ContinuousLetterLength.java
@@ -9,7 +9,7 @@ import static java.lang.Integer.parseInt;
 import static java.lang.System.in;
 
 /**
- * 连续字母长度
+ * 【算法题】连续字母长度
  * <p>
  * AABAAA
  * 2
@@ -25,23 +25,30 @@ public class ContinuousLetterLength {
         int k = parseInt(bf.readLine()); // 查看的第几个长度
         bf.close();
 
-        // 结果收集容器
-        Map<Character, Integer> map = new HashMap<>();
-        // 中间变量
-        Deque<Character> chars;
-
-        for (int i = 0; i < str.length(); ) {
-            chars = new LinkedList<>();
-            while (i < str.length() && (chars.isEmpty() || chars.contains(str.charAt(i)))) { // ((有效范围内&&(第一字符时||还是这个字符时))收集此字符
-                chars.addLast(str.charAt(i));
-                i = i + 1; // 当前字符处理完毕，跳到下个字符坐标
-            }
-
-            Integer tempMax = map.getOrDefault(chars.getFirst(), 0); // 取出当前字符已知的最大长度
-            map.put(chars.getFirst(), Math.max(tempMax, chars.size())); // 动态算取最大长度
-        }
+        Map<Character, Integer> map = getCharAndSizeMap(str);
 
         // 输出结果
         map.values().stream().sorted(Comparator.reverseOrder()).skip(k - 1).limit(1).forEach(System.out::println);
+    }
+
+    /**
+     * [算法题]连续字母长度
+     */
+    private static Map<Character, Integer> getCharAndSizeMap(String str) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        Deque<Character> dynamicChars;
+        for (int i = 0; i < str.length(); ) {
+            dynamicChars = new LinkedList<>();
+            while (i < str.length() && (dynamicChars.isEmpty() || dynamicChars.contains(str.charAt(i)))) { // ((有效范围内&&(第一字符时||还是这个字符时))收集此字符
+                dynamicChars.addLast(str.charAt(i));
+                i = i + 1; // 当前字符处理完毕，跳到下个字符坐标
+            }
+
+            Integer tempMax = map.getOrDefault(dynamicChars.getFirst(), 0); // 取出当前字符已知的最大长度
+            map.put(dynamicChars.getFirst(), Math.max(tempMax, dynamicChars.size())); // 动态算取最大长度
+        }
+
+        return map;
     }
 }
