da276af1ea9e9d569813d89be35f9678813d8aca
zhangziwa <1049505015@qq.com>
2023-03-01 12:20:04 +0800

    【JVM】学习素材
    Took 1 hour 33 minutes

---
+++ business-prac/src/main/javabasic/jvm/Jvm4_Xss.java
@@ -0,0 +1,19 @@
+package jvm;
+
+public class Jvm4_Xss {
+    private static int count;
+
+    private static void recursion() {
+        count++;
+        recursion();
+    }
+
+    public static void main(String[] args) {
+        try {
+            recursion();
+        } catch (Throwable e) {
+            System.out.println("调用最大深度：" + count);
+            e.printStackTrace();
+        }
+    }
+}
