ab5e4a8da2c469c4a01e0dc18a221a43a2d38fbd
zhangziwa <1049505015@qq.com>
2023-03-01 12:20:17 +0800

    【泛型】通配符?的理解
    Took 12 seconds

---
+++ business-prac/src/main/javabasic/generic/wildcard/Message.java
@@ -0,0 +1,11 @@
+package generic.wildcard;
+
+class Message<T> {
+    private T message;
+    public T getMessage() {
+        return message;
+    }
+    public void setMessage(T message) {
+        this.message = message;
+    }
+}
