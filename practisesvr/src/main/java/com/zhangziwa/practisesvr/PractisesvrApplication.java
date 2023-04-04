93827bb4ccabfe58b60b98829635c2f91e694228
zhangziwa <1049505015@qq.com>
2023-04-04 22:39:16 +0800

    【springboot-迭代丰富功能】新追加springboot项目
    Took 9 hours 10 minutes

---
+++ practisesvr/src/main/java/com/zhangziwa/practisesvr/PractisesvrApplication.java
@@ -0,0 +1,13 @@
+package com.zhangziwa.practisesvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractisesvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PractisesvrApplication.class, args);
+    }
+
+}
---
+++ practisesvr/src/test/java/com/zhangziwa/practisesvr/PractisesvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangziwa.practisesvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractisesvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
