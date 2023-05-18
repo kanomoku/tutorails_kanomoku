4d0cac83417a3c59d85468149ed54106bc11310f
zhangziwa <1049505015@qq.com>
2023-05-18 18:50:54 +0800

    【多线程】小练习-地主长工
    Took 15 minutes

---
+++ business-prac/src/main/javabasic/thread/demo/Worker.java
@@ -0,0 +1,22 @@
+package thread.demo;
+
+import java.util.concurrent.Callable;
+
+/**
+ * 长工类
+ */
+public class Worker implements Callable<Integer> {
+    private int hours = 3;
+    private int amount;
+
+    @Override
+    public Integer call() throws Exception {
+        while (hours > 0) {
+            System.out.println(Thread.currentThread().getName() + " I'm still working on it......");
+            amount += 100;
+            hours--;
+            Thread.sleep(1000);
+        }
+        return amount;
+    }
+}
---
+++ business-prac/src/main/javabasic/thread/demo/WorkerMainTest.java
@@ -0,0 +1,23 @@
+package thread.demo;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * 功能概要：理解FutureTask的作用
+ */
+public class WorkerMainTest {
+    public static void main(String args[]) throws ExecutionException, InterruptedException {
+
+        Worker worker = new Worker();
+        FutureTask<Integer> farmer = new FutureTask<>(worker);//FutureTask<String>是一个包装器，它通过接受Callable<String>来创建，它同时实现了Future和Runnable接口
+        new Thread(farmer, "闰土: ").start();
+
+        while (!farmer.isDone()) {
+            System.out.println(Thread.currentThread().getName() + ": 看长工做完了没...");
+            Thread.sleep(500);// 地主也就是main线程
+        }
+
+        System.out.println("工作做完了,上交了" + farmer.get() + "斤粮食");
+    }
+}
