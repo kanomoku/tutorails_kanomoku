b15c2f67ac3b44fa1211ea5ff9d8f0d5231950ba
zhangziwa <1049505015@qq.com>
2023-05-19 21:14:11 +0800

    【多线程】线程池-缓存型线程池 CachedThreadPool
    Took 3 minutes

---
+++ business-prac/src/main/javabasic/thread/pool/newCachedThreadPool/CachedThreadPool.java
@@ -0,0 +1,27 @@
+package thread.pool.newCachedThreadPool;
+
+import thread.pool.custom.Task;
+
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPool {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        System.out.println(Thread.currentThread().getName() + "线程: Start at: " + new Date());
+
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println("添加了第" + i + "个任务类");
+            Thread.sleep(2);
+            exec.execute(new Task("线程名字" + i));
+        }
+
+        exec.shutdown();
+
+        System.out.println(Thread.currentThread().getName() + " 线程: Finished all threads at：" + new Date());
+    }
+}
