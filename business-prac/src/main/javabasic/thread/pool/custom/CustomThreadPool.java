7a6b392cc0b92999fb4a1c8be1fcca68694e2de2
zhangziwa <1049505015@qq.com>
2023-05-19 19:08:06 +0800

    【多线程】线程池-自定义线程池
    Took 55 minutes

---
+++ business-prac/src/main/javabasic/thread/pool/custom/CustomThreadPool.java
@@ -0,0 +1,39 @@
+package thread.pool.custom;
+
+import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class CustomThreadPool {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + "线程: Start at: " + new Date());
+
+        // 创建等待队列
+        BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(20);
+
+        // 创建线程池、池中保存的线程数为3，允许的最大线程数为5
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(3, 5, 50, TimeUnit.MILLISECONDS, blockingQueue);
+
+        // 创建七个任务
+        // 每个任务会在一个线程上执行
+        for (int i = 1; i < 10; i++) {
+            System.out.println("添加了第" + i + "个任务类");
+            pool.execute(new Task("线程任务" + i));
+        }
+
+        // 关闭线程池
+        pool.shutdown();
+
+        System.out.println(Thread.currentThread().getName() + "线程: 打卡：" + new Date());
+
+        long i = 0;
+        while (!pool.isTerminated()) {
+            // wait for all tasks to finish
+            i++;
+        }
+
+        System.out.println(Thread.currentThread().getName() + "线程: Finished all threads at：" + new Date() + ". isTerminated 判断次数 " + i);
+    }
+}
---
+++ business-prac/src/main/javabasic/thread/pool/custom/Task.java
@@ -0,0 +1,26 @@
+package thread.pool.custom;
+
+import java.util.Date;
+
+public class Task implements Runnable {
+    private final String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " → " + name + " Start Time = " + new Date());
+        processCommand();
+        System.out.println(Thread.currentThread().getName() + " → " + name + " End   Time = " + new Date());
+    }
+
+    private void processCommand() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
