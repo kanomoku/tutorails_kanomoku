261fb6e311519908321a38430db3eae16cb5816a
zhangziwa <1049505015@qq.com>
2023-05-20 13:07:31 +0800

    【多线程】线程池-周期性线程池 scheduledThreadPoolExecutor.schedule
    Took 2 minutes

--- business-prac/src/main/javabasic/thread/pool/newScheduledThreadPool/Schedule.java
+++ business-prac/src/main/javabasic/thread/pool/newScheduledThreadPool/Schedule.java
@@ -1,4 +1,6 @@
-package thread.pool;
+package thread.pool.newScheduledThreadPool;
+
+import thread.pool.custom.Task;
 
 import java.util.Date;
 import java.util.concurrent.ExecutionException;
@@ -6,27 +8,26 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-public class threadPool6_ScheduledThreadPool {
+public class Schedule {
     public static void main(String args[]) throws InterruptedException, ExecutionException {
 
-        System.out.println(Thread.currentThread().getName() + "线程: Starting at: " + new Date());
+        System.out.println(Thread.currentThread().getName() + "线程: Start at: " + new Date());
 
-        ScheduledThreadPoolExecutor exec = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(5); // 创建大小为5的线程池
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = (ScheduledThreadPoolExecutor) Executors.newScheduledThreadPool(5);
 
         for (int i = 0; i < 10; i++) {
             System.out.println("添加了第" + i + "个线程任务 " + new Date());
-            exec.schedule(new threadPool1_Handle("线程名字" + i), 10, TimeUnit.SECONDS);// 延迟10秒执行
+            scheduledThreadPoolExecutor.schedule(new Task("线程名字" + i), 10, TimeUnit.SECONDS);// 延迟10秒执行
         }
 
-        exec.shutdown(); // 执行到此处并不会马上关闭线程池
+        scheduledThreadPoolExecutor.shutdown();
 
         System.out.println(Thread.currentThread().getName() + "线程: 打卡" + new Date());
 
-        while (!exec.isTerminated()) {
+        while (!scheduledThreadPoolExecutor.isTerminated()) {
             // wait for all tasks to finish
         }
 
         System.out.println(Thread.currentThread().getName() + "线程: Finished all threads at：" + new Date());
     }
-// 实现每个放入的线程延迟10秒执行。
 }
