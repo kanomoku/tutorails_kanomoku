e74d1af6ff49d3db5a0f08d159f9835bcbdca894
zhangziwa <1049505015@qq.com>
2023-05-20 20:05:08 +0800

    【多线程】线程池-周期性线程池 scheduleAtFixedRate
    Took 7 minutes

---
+++ business-prac/src/main/javabasic/thread/pool/newScheduledThreadPool/FixedRate.java
@@ -0,0 +1,38 @@
+package thread.pool.newScheduledThreadPool;
+
+import thread.pool.custom.Task;
+
+import java.util.Date;
+import java.util.concurrent.*;
+
+import static java.lang.Thread.sleep;
+
+public class FixedRate {
+    public static void main(String args[]) throws InterruptedException, ExecutionException {
+        way1();
+        way2();
+    }
+
+    private static void way2() {
+        System.out.println(Thread.currentThread().getName() + "线程: Starting at: " + new Date());
+
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(1);
+        scheduledThreadPoolExecutor.scheduleAtFixedRate(() -> {
+            try {
+                System.out.println(Thread.currentThread().getName() + " → " + " Start Time = " + new Date());
+                sleep(1000);
+                System.out.println(Thread.currentThread().getName() + " → " + " End   Time = " + new Date());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, 1, 4, TimeUnit.SECONDS);
+    }
+
+    private static void way1() {
+        System.out.println(Thread.currentThread().getName() + "线程: Starting at: " + new Date());
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+        scheduledExecutorService.scheduleAtFixedRate(new Task("任务"), 1, 4, TimeUnit.SECONDS); // 初始延时 1s， 周期4s
+    }
+}
