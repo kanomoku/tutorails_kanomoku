26e0d6aa7b3ec6a76f3d1a4a7b012294a4684d80
zhangziwa <1049505015@qq.com>
2023-05-21 17:27:34 +0800

    【多线程】线程池-如何将任务委托给一个ExecutorService
    Took 1 hour 5 minutes

---
+++ business-prac/src/main/javabasic/thread/pool/newCustomThreadPool/TaskC.java
@@ -0,0 +1,30 @@
+package thread.pool.newCustomThreadPool;
+
+import java.util.Date;
+import java.util.concurrent.Callable;
+
+/**
+ * 功能概要：缓冲线程池实例-submit运行
+ */
+public class TaskC implements Callable<String> {
+    private final String name;
+
+    public TaskC(String name) {
+        this.name = name;
+    }
+
+    public String call() throws Exception {
+        System.out.println(Thread.currentThread().getName() + " → " + name + " Start Time = " + new Date());
+        processCommand();
+        System.out.println(Thread.currentThread().getName() + " → " + name + " End   Time = " + new Date());
+        return "返回结果" + Thread.currentThread().getName() + " → " + name;
+    }
+
+    private void processCommand() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
