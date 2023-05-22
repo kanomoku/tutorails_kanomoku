7b0d9f7f0103af29c0f24aed4d27e0e5fea5c553
zhangziwa <1049505015@qq.com>
2023-05-22 20:54:38 +0800

    【多线程】小练习-关于线程安全的演示小例子
    Took 12 minutes

--- business-prac/src/main/javabasic/thread/unsafe/RunnableSellTickets.java
+++ business-prac/src/main/javabasic/thread/unsafe/RunnableSellTickets.java
@@ -5,6 +5,32 @@ public class RunnableSellTickets implements Runnable {
 
     @Override
     public void run() {
+//        unsafe();
+        safe();
+    }
+
+    /**
+     * 线程不安全
+     */
+    private void unsafe() {
+        while (true) {
+            if (ticket > 0) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + "  车票第" + ticket-- + "张");
+            } else {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 线程安全
+     */
+    private void safe() {
         synchronized (this) {
             while (true) {
                 if (ticket > 0) {
@@ -20,20 +46,4 @@ public class RunnableSellTickets implements Runnable {
             }
         }
     }
-
-//    @Override
-//    public void run() {
-//        while (true) {
-//            if (ticket > 0) {
-//                try {
-//                    Thread.sleep(2000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                System.out.println(Thread.currentThread().getName() + "  车票第" + ticket-- + "张");
-//            } else {
-//                break;
-//            }
-//        }
-//    }
 }
