0809081843c244f99b14003a1831e5ddd2783f99
zhangziwa <1049505015@qq.com>
2023-05-29 23:12:36 +0800

    【多线程】多线程-线程方法-isInterrupted
    Took 16 minutes

---
+++ business-prac/src/main/javabasic/thread/threadmethod/isInterrupted/ThreadisInterrupted.java
@@ -0,0 +1,20 @@
+package thread.threadmethod.isInterrupted;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadisInterrupted {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            while (true) {
+            //do nothing, just empty loop.
+            }
+        });
+        thread.start();
+
+        TimeUnit.MILLISECONDS.sleep(2);
+
+        System.out.printf("Thread is interrupted ? %s\n", thread.isInterrupted());
+        thread.interrupt();
+        System.out.printf("Thread is interrupted ? %s\n", thread.isInterrupted());
+    }
+}
