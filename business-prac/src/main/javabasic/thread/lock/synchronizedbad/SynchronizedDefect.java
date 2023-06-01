7543750b02609598e1817d7400fefaa3455b769a
zhangziwa <1049505015@qq.com>
2023-06-01 20:32:46 +0800

    【多线程】多线程-synchronized关键字的缺陷-Java高并发编程详解：多线程与架构设计 作者：汪文君
    Took 24 minutes

---
+++ business-prac/src/main/javabasic/thread/lock/synchronizedbad/SynchronizedDefect.java
@@ -0,0 +1,25 @@
+package thread.lock.synchronizedbad;
+
+import java.util.concurrent.TimeUnit;
+
+// 读书笔记摘自书名：Java高并发编程详解：多线程与架构设计 作者：汪文君
+public class SynchronizedDefect {
+    public synchronized void syncMethod() {
+        try {
+            TimeUnit.HOURS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SynchronizedDefect defect = new SynchronizedDefect();
+        Thread t1 = new Thread(defect::syncMethod, "T1");
+        //make sure the t1 started.
+        t1.start();
+
+        TimeUnit.MILLISECONDS.sleep(2);
+        Thread t2 = new Thread(defect::syncMethod, "T2");
+        t2.start();
+    }
+}
---
+++ business-prac/src/main/javabasic/thread/lock/synchronizedbad/SynchronizedDefect2.java
@@ -0,0 +1,27 @@
+package thread.lock.synchronizedbad;
+
+import java.util.concurrent.TimeUnit;
+
+// 读书笔记摘自书名：Java高并发编程详解：多线程与架构设计 作者：汪文君
+public class SynchronizedDefect2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        SynchronizedDefect defect = new SynchronizedDefect();
+
+        Thread t1 = new Thread(defect::syncMethod, "T1");
+        t1.start();
+
+        //make sure the t1 started.
+        TimeUnit.MILLISECONDS.sleep(2);
+
+        Thread t2 = new Thread(defect::syncMethod, "T2");
+        t2.start();
+
+        //make sure the t2 started.
+        TimeUnit.MILLISECONDS.sleep(2);
+
+        t2.interrupt();
+        System.out.println(t2.isInterrupted());
+        System.out.println(t2.getState());
+    }
+}
