ad01790a2ff9de3ef3214a2a89114d71355ddff9
zhangziwa <1049505015@qq.com>
2023-07-19 22:19:48 +0800

    【Date】 1.TimestampSameInDifferentZone 2.new Date() 3.new Timestamp() 4.new Instant 5.new LocalDateTime 6.new OffsetDateTime 7.new ZonedDateTime
    Took 26 seconds
    
    Took 1 minute

--- date-prac/src/main/java/DateBuilder.java
+++ date-prac/src/main/java/DateBuilder.java
@@ -21,55 +21,70 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
-public class Build {
-    public static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2019, 7, 7, 20, 18, 18, 888);
+import static java.time.Instant.now;
+
+public class DateBuilder {
     public static final ZonedDateTime ZONED_DATE_TIME = ZonedDateTime.of(2019, 7, 7, 20, 18, 18, 888, ZoneId.of("Asia/Tokyo"));
     public static final OffsetDateTime OFFSET_DATE_TIME = OffsetDateTime.of(2019, 7, 7, 20, 18, 18, 888, ZoneOffset.ofHours(9));
+    public static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2019, 7, 7, 20, 18, 18, 888);
     protected static final Date DATE = new Date(1562501898000L);
 
+    /**
+     * 时间戳 是不会跟着 时区 的改变而改变。
+     */
     @Test
-    public void instantBuild() {
-        System.out.println(Instant.now());
-        System.out.println("----------");
-
-        System.out.println(Instant.ofEpochMilli(1562501898000L));//2019-07-07T12:18:18Z
-        System.out.println(Instant.ofEpochMilli(1562501898888L));//2019-07-07T12:18:18.888Z
-        System.out.println(Instant.ofEpochSecond(1562501898));//2019-07-07T12:18:18Z
-        System.out.println(Instant.ofEpochSecond(1562501898,888));//2019-07-07T12:18:18.000000888Z
-        System.out.println("----------");
-        System.out.println(ZONED_DATE_TIME.toInstant());//2019-07-07T11:18:18.000000888Z
-        System.out.println(OFFSET_DATE_TIME.toInstant());//2019-07-07T11:18:18.000000888Z
-        System.out.println(LOCAL_DATE_TIME.toInstant(ZoneOffset.ofHours(8)));//2019-07-07T12:18:18.000000888Z
-        System.out.println(LOCAL_DATE_TIME.toInstant(ZoneOffset.UTC));//2019-07-07T20:18:18.000000888Z
-        System.out.println("----------");
-        System.out.println(DATE.toInstant());//2019-07-07T12:18:18Z
-        System.out.println("----------");
-
-        System.out.println(Instant.parse("2019-07-07T20:18:18.000000888Z"));//2019-07-07T20:18:18.000000888Z
-        System.out.println(Instant.parse("2019-07-07T20:18:18Z"));//2019-07-07T20:18:18Z
-        System.out.println("----------");
+    public void TimestampSameInDifferentZone() {
+        System.out.println(ZonedDateTime.now().toEpochSecond()); // 1689775687
+        System.out.println(ZonedDateTime.now(ZoneId.of("Asia/Tokyo")).toEpochSecond()); // 1689775687
+        System.out.println(ZonedDateTime.now(ZoneId.of("Asia/Shanghai")).toEpochSecond()); // 1689775687
+        System.out.println(Instant.now().atZone(ZoneOffset.UTC).toEpochSecond()); // 1689775687
+        System.out.println(new Date().getTime()); // 1689775687682
     }
 
+    /**
+     * 如何构造一个 Date()
+     */
     @Test
     public void dateBuild() throws ParseException {
-        System.out.println(new Date(1562501898000L));//Sun Jul 07 20:18:18 CST 2019
-        System.out.println(new Date());
+        System.out.println(new Date(1562501898000L)); // Sun Jul 07 20:18:18 CST 2019
+        System.out.println(new Date(1689775687682L)); // Wed Jul 19 22:08:07 CST 2023
+        System.out.println(new Date(1689775687L)); // Tue Jan 20 21:22:55 CST 1970、 toEpochSecond和Date一起使用要注意
+        System.out.println(new Date()); // Wed Jul 19 22:21:49 CST 2023
+        System.out.println("A----------");
 
+        System.out.println(new Timestamp(1562501898000L)); // 2019-07-07 20:18:18.0
         Date date1 = new Timestamp(1562501898000L);//2019-07-07 20:18:18.0
         System.out.println(date1);
         System.out.println(new Date(new Timestamp(1562501898000L).getTime()));//Sun Jul 07 20:18:18 CST 2019
+        System.out.println("B----------");
 
-        System.out.println(Calendar.getInstance().getTime());
-        System.out.println("----------");
+        System.out.println(Calendar.getInstance().getTime()); // Wed Jul 19 22:35:14 CST 2023
+        System.out.println("C----------");
+
+        System.out.println(Date.from(Instant.ofEpochMilli(1562501898888L)));//Sun Jul 07 20:18:18 CST 2019
+        System.out.println(Date.from(Instant.ofEpochSecond(1562501898)));//Sun Jul 07 20:18:18 CST 2019
+        System.out.println(Date.from(Instant.ofEpochSecond(1562501898, 888)));//Sun Jul 07 20:18:18 CST 2019
+        System.out.println(Date.from(Instant.parse("2019-07-07T20:18:18.000000888Z")));//Mon Jul 08 04:18:18 CST 2019
+        System.out.println(Date.from(Instant.parse("2019-07-07T20:18:18Z")));//Mon Jul 08 04:18:18 CST 2019
+        System.out.println("D----------");
+
+        System.out.println(Date.from(ZONED_DATE_TIME.toInstant()));//Sun Jul 07 19:18:18 CST 2019
+        System.out.println(Date.from(OFFSET_DATE_TIME.toInstant()));//Sun Jul 07 19:18:18 CST 2019
+        System.out.println(Date.from(LOCAL_DATE_TIME.toInstant(ZoneOffset.UTC)));//Mon Jul 08 04:18:18 CST 2019
+        System.out.println(Date.from(LOCAL_DATE_TIME.toInstant(ZoneOffset.ofHours(8))));//Sun Jul 07 20:18:18 CST 2019
+        System.out.println("E----------");
+
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println(
-            "格式化器用的时区是：" + dateFormat.getTimeZone().getID() + ":" + dateFormat.parse("2019-07-07 20:18:18"));
+        System.out.println("格式化器用的时区是：" + dateFormat.getTimeZone().getID() + ":" + dateFormat.parse("2019-07-07 20:18:18"));
         dateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"));
-        System.out.println(
-            "格式化器用的时区是：" + dateFormat.getTimeZone().getID() + ":" + dateFormat.parse("2019-07-07 20:18:18"));
+        System.out.println("格式化器用的时区是：" + dateFormat.getTimeZone().getID() + ":" + dateFormat.parse("2019-07-07 20:18:18"));
+        System.out.println("F----------");
 
-        System.out.println("---1-------");
+        System.out.println(DateUtils.parseDate("2019-07-32 20:18:18 +0800", "yyyy-MM-dd HH:mm:ss Z"));//Thu Aug 01 20:18:18 CST 2019
+        System.out.println(DateUtils.parseDateStrictly("2019-07-07 20:18:18 +0800", "yyyy-MM-dd HH:mm:ss Z")); //Sun Jul 07 20:18:18 CST 2019
+        System.out.println("J----------");
+
         System.out.println(DateUtils.parseDate("2019-7-07 20:18:18.888 +0800", "yyyy-MM-dd HH:mm:ss.SSS Z"));
         System.out.println(DateUtils.parseDate("2019-7-07 20:18:18 +0800", "yyyy-MM-dd HH:mm:ss Z"));
         System.out.println(DateUtils.parseDate("2019-7-07 20:18:18.888", "yyyy-MM-dd HH:mm:ss.SSS"));
@@ -79,7 +94,7 @@ public class Build {
         System.out.println(DateUtils.parseDate("2019-7-07", "yyyy-MM-dd"));
         System.out.println(DateUtils.parseDate("2019-7", "yyyy-MM"));
         System.out.println(DateUtils.parseDate("2019", "yyyy"));
-        System.out.println("---2-------");
+        System.out.println("G----------");
         System.out.println(DateUtils.parseDate("2019/7/07 20:18:18.888 +0800", "yyyy/MM/dd HH:mm:ss.SSS Z"));
         System.out.println(DateUtils.parseDate("2019/7/07 20:18:18 +0800", "yyyy/MM/dd HH:mm:ss Z"));
         System.out.println(DateUtils.parseDate("2019/7/07 20:18:18.888", "yyyy/MM/dd HH:mm:ss.SSS"));
@@ -89,7 +104,7 @@ public class Build {
         System.out.println(DateUtils.parseDate("2019/7/07", "yyyy/MM/dd"));
         System.out.println(DateUtils.parseDate("2019/7", "yyyy/MM"));
         System.out.println(DateUtils.parseDate("2019", "yyyy"));
-        System.out.println("---3-------");
+        System.out.println("H----------");
         System.out.println(DateUtils.parseDate("20190707 20:18:18.888 +0800", "yyyyMMdd HH:mm:ss.SSS Z"));
         System.out.println(DateUtils.parseDate("20190707 20:18:18 +0800", "yyyyMMdd HH:mm:ss Z"));
         System.out.println(DateUtils.parseDate("20190707 20:18:18.888", "yyyyMMdd HH:mm:ss.SSS"));
@@ -99,21 +114,33 @@ public class Build {
         System.out.println(DateUtils.parseDate("20190707", "yyyyMMdd"));
         System.out.println(DateUtils.parseDate("201907", "yyyyMM"));
         System.out.println(DateUtils.parseDate("2019", "yyyy"));
-        System.out.println("---4-------");
-        System.out.println(
-            DateUtils.parseDate("2019-07-32 20:18:18 +0800", "yyyy-MM-dd HH:mm:ss Z"));//Thu Aug 01 20:18:18 CST 2019
-        System.out.println(DateUtils.parseDateStrictly("2019-07-07 20:18:18 +0800", "yyyy-MM-dd HH:mm:ss Z"));
-        System.out.println("---5-------");
-        System.out.println(Date.from(Instant.ofEpochMilli(1562501898888L)));//Sun Jul 07 20:18:18 CST 2019
-        System.out.println(Date.from(Instant.ofEpochSecond(1562501898, 888)));//Sun Jul 07 20:18:18 CST 2019
-        System.out.println(Date.from(Instant.ofEpochSecond(1562501898)));//Sun Jul 07 20:18:18 CST 2019
-        System.out.println(Date.from(Instant.parse("2019-07-07T20:18:18.000000888Z")));//Mon Jul 08 04:18:18 CST 2019
-        System.out.println(Date.from(Instant.parse("2019-07-07T20:18:18Z")));//Mon Jul 08 04:18:18 CST 2019
-        System.out.println("---6-------");
-        System.out.println(Date.from(ZONED_DATE_TIME.toInstant()));//Sun Jul 07 19:18:18 CST 2019
-        System.out.println(Date.from(OFFSET_DATE_TIME.toInstant()));//Sun Jul 07 19:18:18 CST 2019
-        System.out.println(Date.from(LOCAL_DATE_TIME.toInstant(ZoneOffset.UTC)));//Mon Jul 08 04:18:18 CST 2019
-        System.out.println(Date.from(LOCAL_DATE_TIME.toInstant(ZoneOffset.ofHours(8))));//Sun Jul 07 20:18:18 CST 2019
+        System.out.println("I----------");
+    }
+
+    @Test
+    public void instantBuild() {
+        System.out.println(Instant.now());// 2023-07-20T04:52:37.670793700Z
+        System.out.println("1----------");
+
+        System.out.println(Instant.ofEpochMilli(1562501898000L));//2019-07-07T12:18:18Z
+        System.out.println(Instant.ofEpochMilli(1562501898888L));//2019-07-07T12:18:18.888Z
+        System.out.println(Instant.ofEpochSecond(1562501898));//2019-07-07T12:18:18Z
+        System.out.println(Instant.ofEpochSecond(1562501898, 888));//2019-07-07T12:18:18.000000888Z
+        System.out.println("2----------");
+
+        System.out.println(ZONED_DATE_TIME.toInstant());//2019-07-07T11:18:18.000000888Z
+        System.out.println(OFFSET_DATE_TIME.toInstant());//2019-07-07T11:18:18.000000888Z
+        System.out.println(LOCAL_DATE_TIME.toInstant(ZoneOffset.ofHours(8)));//2019-07-07T12:18:18.000000888Z
+        System.out.println(LOCAL_DATE_TIME.toInstant(ZoneOffset.UTC));//2019-07-07T20:18:18.000000888Z
+        System.out.println("3----------");
+
+        System.out.println(DATE);// Sun Jul 07 20:18:18 CST 2019
+        System.out.println(DATE.toInstant());//2019-07-07T12:18:18.888Z
+        System.out.println("4----------");
+
+        System.out.println(Instant.parse("2019-07-07T20:18:18.000000888Z"));//2019-07-07T20:18:18.000000888Z
+        System.out.println(Instant.parse("2019-07-07T20:18:18Z"));//2019-07-07T20:18:18Z
+        System.out.println("-5---------");
     }
 
     @Test
@@ -123,8 +150,8 @@ public class Build {
         System.out.println("----------");
 
         System.out.println(Timestamp.from(Instant.ofEpochMilli(1562501898888L)));//2019-07-07 20:18:18.888
-        System.out.println(Timestamp.from(Instant.ofEpochSecond(1562501898,888)));//2019-07-07 20:18:18.000000888
         System.out.println(Timestamp.from(Instant.ofEpochSecond(1562501898)));//2019-07-07 20:18:18.0
+        System.out.println(Timestamp.from(Instant.ofEpochSecond(1562501898, 888)));//2019-07-07 20:18:18.000000888
         System.out.println(Timestamp.from(Instant.parse("2019-07-07T20:18:18.000000888Z")));//2019-07-08 04:18:18.000000888
         System.out.println(Timestamp.from(Instant.parse("2019-07-07T20:18:18Z")));//2019-07-08 04:18:18.0
         System.out.println("----------");
@@ -177,13 +204,13 @@ public class Build {
         System.out.println(LocalDateTime.of(2019, Month.JULY, 7, 20, 18, 18, 888));//2019-07-07T20:18:18.000000888
         System.out.println(LocalDateTime.of(2019, Month.JULY, 7, 20, 18, 18));//2019-07-07T20:18:18
         System.out.println(LocalDateTime.of(2019, Month.JULY, 7, 20, 18));//2019-07-07T20:18
-        System.out.println(LocalDateTime.of(LocalDate.of(2019, 7, 7), LocalTime.of(20, 18,18,888)));//2019-07-07T20:18:18.000000888
-        System.out.println(LocalDateTime.of(LocalDate.of(2019, 7, 7), LocalTime.of(20, 18,18)));//2019-07-07T20:18:18
+        System.out.println(LocalDateTime.of(LocalDate.of(2019, 7, 7), LocalTime.of(20, 18, 18, 888)));//2019-07-07T20:18:18.000000888
+        System.out.println(LocalDateTime.of(LocalDate.of(2019, 7, 7), LocalTime.of(20, 18, 18)));//2019-07-07T20:18:18
         System.out.println(LocalDateTime.of(LocalDate.of(2019, 7, 7), LocalTime.of(20, 18)));//2019-07-07T20:18
         System.out.println("----------2");
 
-        System.out.println(LocalDateTime.ofEpochSecond(1562501898,888888888, ZoneOffset.UTC));//2019-07-07T12:18:18.888888888
-        System.out.println(LocalDateTime.ofEpochSecond(1562501898,888888888, ZoneOffset.ofHours(8)));//2019-07-07T20:18:18.888888888
+        System.out.println(LocalDateTime.ofEpochSecond(1562501898, 888888888, ZoneOffset.UTC));//2019-07-07T12:18:18.888888888
+        System.out.println(LocalDateTime.ofEpochSecond(1562501898, 888888888, ZoneOffset.ofHours(8)));//2019-07-07T20:18:18.888888888
         System.out.println(LocalDateTime.ofInstant(Instant.parse("2019-07-07T20:18:18.000000888Z"), ZoneId.of("UTC")));//2019-07-07T20:18:18.000000888
         System.out.println(LocalDateTime.ofInstant(Instant.parse("2019-07-07T20:18:18.000000888Z"), ZoneId.of("+08:00")));//2019-07-08T04:18:18.000000888
 
@@ -195,9 +222,9 @@ public class Build {
         System.out.println(LocalDateTime.parse("2022-04-03T18:00"));//2022-04-03T18:00
 
         DateTimeFormatter df = DateTimeFormatter.ofPattern("第Q季度 yyyy-MM-dd HH:mm:ss", Locale.JAPAN);
-        System.out.println(LocalDateTime.parse("第3季度 2019-07-07 20:18:18",df));//2019-07-07T20:18:18
+        System.out.println(LocalDateTime.parse("第3季度 2019-07-07 20:18:18", df));//2019-07-07T20:18:18
         DateTimeFormatter df1 = DateTimeFormatter.ofPattern("第Q季度 yyyy-MM-dd HH:mm:ss", Locale.CHINA);
-        System.out.println(LocalDateTime.parse("第3季度 2019-07-07 20:18:18",df1));//2019-07-07T20:18:18
+        System.out.println(LocalDateTime.parse("第3季度 2019-07-07 20:18:18", df1));//2019-07-07T20:18:18
 
         System.out.println(LocalDateTime.parse("2019-07-07T20:18:18.000000888"));//2019-07-07T20:18:18.000000888
         System.out.println(LocalDateTime.parse("2019-07-07T20:18:18"));//2019-07-07T20:18:18
@@ -206,19 +233,24 @@ public class Build {
 
     @Test
     public void offsetDateTimeBuild() {
-        System.out.println(OffsetDateTime.now());// 使用系统时区 2022-04-03T17:30:32.209357700+08:00
-        System.out.println(OffsetDateTime.now(ZoneId.of("Asia/Tokyo")));//自己指定时区 2022-04-03T18:30:32.209357700+09:00
-        System.out.println(OffsetDateTime.now(Clock.systemUTC()));//自己指定时区 2022-04-03T09:30:32.209357700Z
+        // 2023-07-20T22:18:02执行代码
+        System.out.println(OffsetDateTime.now());// 使用系统时区 2023-07-20T22:18:02.831578900+08:00
+        System.out.println(OffsetDateTime.now(ZoneId.of("Asia/Tokyo")));//自己指定时区 2023-07-20T23:18:02.831578900+09:00
+        System.out.println(OffsetDateTime.now(Clock.systemUTC()));//自己指定时区 2023-07-20T14:18:02.831578900Z
         System.out.println("----------1");
 
         System.out.println(OffsetDateTime.of(2019, 7, 7, 20, 18, 18, 888, ZoneOffset.ofHours(9)));//2019-07-07T20:18:18.000000888+09:00
-        System.out.println(OffsetDateTime.of(LocalDate.of(2019, 7, 7),LocalTime.of(20, 18, 18, 888),ZoneOffset.ofHours(9)));//2019-07-07T20:18:18.000000888+09:00
-        System.out.println(OffsetDateTime.of(LOCAL_DATE_TIME,ZoneOffset.ofHours(9)));//2019-07-07T20:18:18.000000888+09:00
+        System.out.println(OffsetDateTime.of(LocalDate.of(2019, 7, 7), LocalTime.of(20, 18, 18, 888), ZoneOffset.ofHours(9)));//2019-07-07T20:18:18.000000888+09:00
+        System.out.println(OffsetDateTime.of(LOCAL_DATE_TIME, ZoneOffset.ofHours(9)));//2019-07-07T20:18:18.000000888+09:00
+        System.out.println(OffsetDateTime.of(LOCAL_DATE_TIME, ZoneOffset.ofHours(8)));//2019-07-07T20:18:18.000000888+08:00
         System.out.println(OffsetDateTime.ofInstant(LOCAL_DATE_TIME.toInstant(ZoneOffset.ofHours(9)), ZoneOffset.ofHours(9)));//2019-07-07T20:18:18.000000888+09:00
+        System.out.println(OffsetDateTime.ofInstant(LOCAL_DATE_TIME.toInstant(ZoneOffset.ofHours(9)), ZoneOffset.ofHours(8)));//2019-07-07T19:18:18.000000888+08:00
         System.out.println("----------2");
 
         System.out.println(LOCAL_DATE_TIME.atOffset(ZoneOffset.ofHours(9)));//2019-07-07T20:18:18.000000888+09:00
+        System.out.println(LOCAL_DATE_TIME.atOffset(ZoneOffset.ofHours(8)));//2019-07-07T20:18:18.000000888+08:00
         System.out.println(ZONED_DATE_TIME.toOffsetDateTime());//2019-07-07T20:18:18.000000888+09:00
+        System.out.println("----------3");
 
         System.out.println(OffsetDateTime.parse("2019-07-07T20:18:18.000000888+09:00"));//2019-07-07T20:18:18.000000888+09:00
         System.out.println(OffsetDateTime.parse("2019-07-07T20:18:18+09:00"));//2019-07-07T20:18:18+09:00
@@ -227,13 +259,14 @@ public class Build {
 
     @Test
     public void zonedDateTimeBuild() {
-        System.out.println(ZonedDateTime.now());//使用系统时区 2022-04-03T17:30:32.193733700+08:00[Asia/Shanghai]
-        System.out.println(ZonedDateTime.now(ZoneId.of("Asia/Tokyo")));//自己指定时区 2022-04-03T18:30:32.193733700+09:00[Asia/Tokyo]
-        System.out.println(ZonedDateTime.now(Clock.systemUTC()));//自己指定时区 2022-04-03T09:30:32.193733700Z
+        // 2023-07-20T22:48:15执行
+        System.out.println(ZonedDateTime.now());//使用系统时区 2023-07-20T22:48:15.845275700+08:00[Asia/Shanghai]
+        System.out.println(ZonedDateTime.now(ZoneId.of("Asia/Tokyo")));//自己指定时区 2023-07-20T23:48:15.845275700+09:00[Asia/Tokyo]
+        System.out.println(ZonedDateTime.now(Clock.systemUTC()));//自己指定时区 2023-07-20T14:48:15.845275700Z
         System.out.println("----------2");
 
         System.out.println(ZonedDateTime.of(2019, 7, 7, 20, 18, 18, 888, ZoneId.of("Asia/Tokyo")));//2019-07-07T20:18:18.000000888+09:00[Asia/Tokyo]
-        System.out.println(ZonedDateTime.of(LocalDate.of(2019, 7, 7),LocalTime.of(20, 18, 18, 888),ZoneId.of("Asia/Tokyo")));//2019-07-07T20:18:18.000000888+09:00[Asia/Tokyo]
+        System.out.println(ZonedDateTime.of(LocalDate.of(2019, 7, 7), LocalTime.of(20, 18, 18, 888), ZoneId.of("Asia/Tokyo")));//2019-07-07T20:18:18.000000888+09:00[Asia/Tokyo]
         System.out.println(ZonedDateTime.of(LOCAL_DATE_TIME, ZoneId.of("Asia/Tokyo")));//2019-07-07T20:18:18.000000888+09:00[Asia/Tokyo]
         System.out.println("--------3");
 
@@ -260,7 +293,7 @@ public class Build {
         System.out.println(ZonedDateTime.parse("2019-07-07T20:18:18+09:00[Asia/Tokyo]"));//2019-07-07T20:18:18+09:00[Asia/Tokyo]
         System.out.println(ZonedDateTime.parse("2019-07-07T20:18+09:00[Asia/Tokyo]"));//2019-07-07T20:18+09:00[Asia/Tokyo]
 
-        System.out.println(ZonedDateTime.parse("Sun Oct 01 00:00:00 EDT 2017",DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy").withLocale(Locale.US)));
-        System.out.println(ZonedDateTime.parse("Sun Oct 01 00:00:00 EDT 2017 GMT+0800",DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy 'GMT'Z").withLocale(Locale.US)));
+        System.out.println(ZonedDateTime.parse("Sun Oct 01 00:00:00 EDT 2017", DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy").withLocale(Locale.US))); // 2017-10-01T00:00-04:00[America/New_York]
+        System.out.println(ZonedDateTime.parse("Sun Oct 01 00:00:00 EDT 2017 GMT+0800", DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy 'GMT'Z").withLocale(Locale.US)));// 2017-09-30T12:00-04:00[America/New_York]
     }
 }
