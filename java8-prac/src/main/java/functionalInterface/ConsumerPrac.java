e5dbe41042eec05e14d7babb168601319fcc703d
zhangziwa <1049505015@qq.com>
2023-07-24 12:47:31 +0800

    【JAVA8】Consumer复习
    Took 40 minutes

--- java8-prac/src/main/java/functionalInterface/ConsumerPrac.java
+++ java8-prac/src/main/java/functionalInterface/ConsumerPrac.java
@@ -8,25 +8,41 @@ import java.util.stream.Stream;
 public class ConsumerPrac {
 
     @Test
-    public void basic(){
+    public void basic() {
         // 1、传统方式
         Consumer<String> consumer = new Consumer<String>() {
             @Override
             public void accept(String s) {
-                System.out.print(s);
+                System.out.println(s);
             }
         };
+
         Stream<String> stream = Stream.of("aaa", "bbb", "ddd", "ccc", "fff");
         stream.forEach(consumer);
 
         // 2、lambda表达式
-        Stream<String> stream1 = Stream.of("aaa", "bbb", "ddd", "ccc", "fff");
         Consumer<String> consumer1 = (s) -> System.out.print(s);
+        Stream<String> stream1 = Stream.of("aaa", "bbb", "ddd", "ccc", "fff");
         stream1.forEach(consumer1);
+
+        stream1.forEach((s) -> System.out.print(s));
 
         // 3、语法糖
         Stream<String> stream2 = Stream.of("aaa", "bbb", "ddd", "ccc", "fff");
         stream2.forEach(System.out::print);
+    }
+
+    @Test
+    public void andThen() {
+        StringBuilder sb = new StringBuilder("Hello ");
+
+        Consumer<StringBuilder> consumer = (str) -> str.append("Jack!");
+        consumer.accept(sb);
+        System.out.println(sb.toString());    // Hello Jack!
+
+        Consumer<StringBuilder> consumer1 = (str) -> str.append(" Bob!");
+        consumer.andThen(consumer1).accept(sb);
+        System.out.println(sb.toString());    // Hello Jack! Bob!
     }
 }
 
