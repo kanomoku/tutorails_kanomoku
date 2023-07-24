f627c7b509ecdc191cefdb70a5b7292b997bed49
zhangziwa <1049505015@qq.com>
2023-07-24 13:16:15 +0800

    【JAVA8】Predicate复习
    Took 23 minutes

--- java8-prac/src/main/java/functionalInterface/PredicatePrac.java
+++ java8-prac/src/main/java/functionalInterface/PredicatePrac.java
@@ -28,4 +28,39 @@ public class PredicatePrac {
         System.out.println(equalPredicate.test(4)); // false
         System.out.println(equalPredicate.test(3)); // true
     }
+
+    /**
+     * 条件取反
+     */
+    @Test
+    public void negate() {
+        Predicate<Integer> predicate = t -> t > 4;
+        System.out.println(predicate.negate().test(5)); // false
+        System.out.println(predicate.negate().test(4)); // true
+        System.out.println(predicate.negate().test(3)); // true
+
+        System.out.println(Predicate.not(predicate).test(5)); // false
+        System.out.println(Predicate.not(predicate).test(4)); // true
+        System.out.println(Predicate.not(predicate).test(3)); // true
+    }
+
+    @Test
+    public void and() {
+        Predicate<Integer> predicate = t -> t > 4;
+        Predicate<Integer> predicate2 = t -> t < 6;
+
+        System.out.println(predicate.and(predicate2).test(3)); // false
+        System.out.println(predicate.and(predicate2).test(5)); // true
+        System.out.println(predicate.and(predicate2).test(7)); // false
+    }
+
+    @Test
+    public void or() {
+        Predicate<Integer> predicate = t -> t >= 6;
+        Predicate<Integer> predicate2 = t -> t <= 4;
+
+        System.out.println(predicate.or(predicate2).test(4)); // true
+        System.out.println(predicate.or(predicate2).test(5)); // false
+        System.out.println(predicate.or(predicate2).test(6)); // true
+    }
 }
