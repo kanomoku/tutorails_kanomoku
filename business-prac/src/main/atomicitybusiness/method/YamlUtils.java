de0187a93caaa331e2926b0638fe63beaee212a5
zhangziwa <1049505015@qq.com>
2023-07-29 11:20:52 +0800

    【Yaml】yaml和map互转
    Took 19 seconds

--- business-prac/src/main/atomicitybusiness/method/YamlUtils.java
+++ business-prac/src/main/atomicitybusiness/method/YamlUtils.java
@@ -7,40 +7,55 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.SafeConstructor;
+import org.yaml.snakeyaml.representer.Representer;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 
 @Slf4j
 public class YamlUtils {
 
-    public static Map<String, Object> fromYamlStringToMap(String str) {
-        Map map = new HashMap<>();
-        if (StringUtils.isBlank(str)) {
-            return map;
+    private YamlUtils() {
+    }
+
+    public static Map<String, Object> yamlToMap(String yamlStr) {
+        if (StringUtils.isBlank(yamlStr)) {
+            return new HashMap<>();
         }
 
+        Map map = new HashMap<>();
         try {
             ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-            map = objectMapper.readValue(str, Map.class);
+            map = objectMapper.readValue(yamlStr, Map.class);
         } catch (JsonProcessingException e) {
-            log.info("log something");
+            log.info("yamlToMap occur error {}", e.getMessage());
         }
         return map;
     }
 
-    public static String fromMapToYamlString(Map<String, Object> map) {
-
-        if (MapUtils.isEmpty(map)) {
-            return "";
+    public static Map<String, Object> yamlToMap2(String yamlStr) {
+        if (StringUtils.isBlank(yamlStr)) {
+            return new HashMap<>();
         }
 
+        Yaml yaml = new Yaml(new SafeConstructor(new LoaderOptions()));
+        return yaml.load(yamlStr);
+    }
+
+    public static String mapToYaml(Map<String, Object> map) {
+        if (MapUtils.isEmpty(map)) {
+            return EMPTY;
+        }
 
         DumperOptions dumperOptions = new DumperOptions();
         dumperOptions.setExplicitStart(true);
         dumperOptions.setExplicitEnd(true);
-        return new Yaml(dumperOptions).dumpAsMap(map);
+        return new Yaml(new SafeConstructor(new LoaderOptions()), new Representer(dumperOptions), dumperOptions).dumpAsMap(map);
     }
 }
--- business-prac/src/test/java/atomicitybusiness/method/YamlUtilsTest.java
+++ business-prac/src/test/java/atomicitybusiness/method/YamlUtilsTest.java
@@ -9,19 +9,28 @@ import java.util.Map;
 public class YamlUtilsTest {
 
     @Test
-    public void fromYamlStringToMap() {
+    public void testYamlToMap() {
         String str = "---\nkey1: value1\nkey2: value2\n...\n";
-        Map<String, Object> map = YamlUtils.fromYamlStringToMap(str);
-        System.out.println(map);
+        Map<String, Object> map = YamlUtils.yamlToMap(str);
         Assert.assertEquals(2, map.size());
+        Assert.assertEquals("{key1=value1, key2=value2}", map.toString());
     }
 
     @Test
-    public void fromMapToYamlString() {
+    public void testYamlToMap2() {
+        String str = "---\nkey1: value1\nkey2: value2\n...\n";
+        Map<String, Object> map = YamlUtils.yamlToMap2(str);
+        System.out.println(map);
+        Assert.assertEquals(2, map.size());
+        Assert.assertEquals("{key1=value1, key2=value2}", map.toString());
+    }
+
+    @Test
+    public void testMapToYaml() {
         Map<String, Object> map = new HashMap<>();
         map.put("key1", "value1");
         map.put("key2", "value2");
-        String resStr = YamlUtils.fromMapToYamlString(map);
+        String resStr = YamlUtils.mapToYaml(map);
         Assert.assertEquals("---\nkey1: value1\nkey2: value2\n...\n", resStr);
     }
 }
