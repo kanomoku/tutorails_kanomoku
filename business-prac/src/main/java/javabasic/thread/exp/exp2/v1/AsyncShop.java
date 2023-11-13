3e6786b9062e0fdfe464f3297eb9d8b6a2271563
zhangziwa <1049505015@qq.com>
2023-11-13 22:06:18 +0800

    【java8+高并发】char10  学习笔记 模拟异步调用发生异常场景 java8的优化写法+定义项目调用异常
    Took 3 minutes
    
    Took 6 minutes

--- business-prac/src/main/java/javabasic/thread/exp/exp2/v1/AsyncShop.java
+++ business-prac/src/main/java/javabasic/thread/exp/exp2/v1/AsyncShop.java
@@ -4,6 +4,7 @@ import javabasic.thread.exp.exp2.Util;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.Future;
 
 public class AsyncShop {
@@ -58,20 +59,20 @@ public class AsyncShop {
 //        return futurePrice;
 //    }
 
-    public Future<Double> getPrice(String product) {
-        return CompletableFuture.supplyAsync(() -> calculatePrice(product));
-    }
-
 //    public Future<Double> getPrice(String product) {
-//        CompletableFuture<Double> futurePrice = CompletableFuture
-//                .supplyAsync(() -> calculatePrice(product))
-//                .exceptionally(e -> {
-//                    CompletionException ex = new CompletionException(e);
-//                    System.out.println("自定义项目调用异常：" + e.getMessage());
-//                    throw ex;
-//                });
-//        return futurePrice;
+//        return CompletableFuture.supplyAsync(() -> calculatePrice(product));
 //    }
+
+    public Future<Double> getPrice(String product) {
+        CompletableFuture<Double> futurePrice = CompletableFuture
+                .supplyAsync(() -> calculatePrice(product))
+                .exceptionally(e -> {
+                    CompletionException ex = new CompletionException(e);
+                    System.out.println("自定义项目调用异常：" + e.getMessage());
+                    throw ex;
+                });
+        return futurePrice;
+    }
 
     private double calculatePrice(String product) {
         Util.delay();
