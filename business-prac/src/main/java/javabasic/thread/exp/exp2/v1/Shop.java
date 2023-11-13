93bba5df8169c7ebc043c243d3e13ee8f802fdb5
zhangziwa <1049505015@qq.com>
2023-11-13 13:55:56 +0800

    【java8+高并发】char10  学习笔记 1、根据产品名称返回价格（同步实现） 2、根据产品名称返回价格（异步实现） 复习
    Took 3 hours 42 minutes

--- business-prac/src/main/java/javabasic/thread/exp/exp2/v1/Shop.java
+++ business-prac/src/main/java/javabasic/thread/exp/exp2/v1/Shop.java
@@ -2,11 +2,13 @@ package javabasic.thread.exp.exp2.v1;
 
 
 import javabasic.thread.exp.exp2.Util;
+import lombok.Data;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
+@Data
 public class Shop {
 
     private final String name;
@@ -25,21 +27,19 @@ public class Shop {
     // 依据指定产品名称返回价格,将同步方法转换为异步方法
     public Future<Double> getPriceAsync(String product) {
         CompletableFuture<Double> futurePrice = new CompletableFuture<>();
+
         new Thread(() -> {
             double price = calculatePrice(product);
-            System.out.println(Thread.currentThread().getName() + " 执行线程任务");
             futurePrice.complete(price); // 如果价格计算正常结束，完成Future操作并设置商品价格
         }).start();
+
         return futurePrice;
     }
 
     private double calculatePrice(String product) {
         Util.delay();
+        System.out.println(Thread.currentThread().getName() + " 线程执行 calculatePrice");
         // 依据产品的名称，生成一个随机值作为价格
         return random.nextDouble() * product.charAt(0) + product.charAt(1);
-    }
-
-    public String getName() {
-        return name;
     }
 }
--- business-prac/src/main/java/javabasic/thread/exp/exp2/v1/ShopMain.java
+++ business-prac/src/main/java/javabasic/thread/exp/exp2/v1/ShopMain.java
@@ -1,5 +1,8 @@
 package javabasic.thread.exp.exp2.v1;
 
+import javabasic.log.StopWatchUtils;
+import org.springframework.util.StopWatch;
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -10,37 +13,47 @@ public class ShopMain {
 //        sync();
     }
 
+    // 同步实现
     private static void sync() {
         Shop shop = new Shop("BestShop");
-        long start = System.nanoTime();
+        StopWatch stopWatch = new StopWatch("串行用时统计");
 
+        stopWatch.start("getPrice");
         double price = shop.getPrice("product name");
-        System.out.println(Thread.currentThread().getName() + " Invocation returned after " + ((System.nanoTime() - start) / 1_000_000) + " msecs");
-
         System.out.printf(Thread.currentThread().getName() + " Price is %.2f%n", price);
+        stopWatch.stop();
 
+        stopWatch.start("doSomethingElse");
+        // 模拟其他业务逻辑
         doSomethingElse();
+        stopWatch.stop();
 
-        System.out.println(Thread.currentThread().getName() + " Price returned after " + ((System.nanoTime() - start) / 1_000_000) + " msecs");
+        StopWatchUtils.logStopWatch(stopWatch);
     }
 
+    // 异步实现
     private static void async() {
         Shop shop = new Shop("BestShop");
-        long start = System.nanoTime();
+        StopWatch stopWatch = new StopWatch("并行用时统计");
 
+        stopWatch.start("getPrice new a thread and start");
         Future<Double> futurePrice = shop.getPriceAsync("product name");
-        System.out.println(Thread.currentThread().getName() + " Invocation returned after " + ((System.nanoTime() - start) / 1_000_000) + " msecs");
+        stopWatch.stop();
 
+        stopWatch.start("doSomethingElse");
         doSomethingElse();
+        stopWatch.stop();
 
+        stopWatch.start("futurePrice.get()");
         try {
             double price = futurePrice.get();
             System.out.printf(Thread.currentThread().getName() + " Price is %.2f%n", price);
         } catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+        stopWatch.stop();
 
-        System.out.println(Thread.currentThread().getName() + " Price returned after " + ((System.nanoTime() - start) / 1_000_000) + " msecs");
+        StopWatchUtils.logStopWatch(stopWatch);
     }
 
 
