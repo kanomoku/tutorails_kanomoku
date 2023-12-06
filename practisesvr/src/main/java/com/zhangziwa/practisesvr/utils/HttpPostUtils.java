4528d3460c84bc9a6d2591232e27b08e4437e667
zhangziwa <1049505015@qq.com>
2023-12-06 23:22:51 +0800

    【最佳实践】模拟解析Http请求返回的Json串 https://blog.csdn.net/weixin_37646636/article/details/134838624
    Took 25 minutes

--- practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/HttpPostUtils.java
+++ practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/HttpPostUtils.java
@@ -1,5 +1,9 @@
 package com.zhangziwa.practisesvr.utils;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.Feature;
+import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Consts;
 import org.apache.http.HttpStatus;
@@ -15,6 +19,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.util.*;
 
 public class HttpPostUtils {
     /**
@@ -60,5 +65,48 @@ public class HttpPostUtils {
             }
         }
         return responseContent;
+    }
+
+    public static List<User> parserJsonToUser() {
+        // 模拟返回的Json串
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("name", Arrays.asList("name1", "name2", "name3"));
+        map.put("age", Arrays.asList("11", "23", "23"));
+        map.put("num", Arrays.asList("123", "234", "345"));
+        String json1 = JsonUtils.toJson(map);
+        System.out.println(json1);
+
+
+        // fastjson
+        JSONObject jsonObject = JSON.parseObject(json1, Feature.OrderedField);
+
+        // 返回的属性
+        List<String> fields = new ArrayList<>(map.keySet());
+        // 几份值
+        int size = jsonObject.getJSONArray(fields.get(0)).size();
+
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            User user1 = new User();
+            for (String field : fields) {
+                Object value = jsonObject.getJSONArray(field).get(i);
+                ReflectUtils.setFieldValue(user1, field, value);
+            }
+            users.add(user1);
+        }
+
+        System.out.println(users);
+        return users;
+    }
+
+    public static void main(String[] args) {
+        parserJsonToUser();
+    }
+
+    @Data
+    static class User {
+        private String name;
+        private String age;
+        private String num;
     }
 }
