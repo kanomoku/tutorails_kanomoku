a5d61db17f712edcf805c155b96b82adbf04a34c
zhangziwa <1049505015@qq.com>
2024-01-11 23:22:25 +0800

    【SpringBoot】@ControllerAdvice 使用场景 https://blog.csdn.net/weixin_37646636/article/details/135500381
    Took 28 minutes

---
+++ practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.zhangziwa.practisesvr.utils.exception;
+
+import org.apache.ibatis.javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found");
+    }
+}
---
+++ practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/response/HttpResponseBodyAdvice.java
@@ -0,0 +1,39 @@
+package com.zhangziwa.practisesvr.utils.response;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import static java.util.Objects.nonNull;
+
+@ControllerAdvice
+public class HttpResponseBodyAdvice implements ResponseBodyAdvice {
+
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class clazz,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        HttpHeaders headers = response.getHeaders();
+
+        // 分页信息添加到ServerHttpResponse
+        HttpHeaders headersContext = ResponseUtils.getHeaders();
+        if (nonNull(headersContext) && !headersContext.isEmpty()) {
+            headers.addAll(headersContext);
+        }
+
+        // 状态码添加到ServerHttpResponse
+        if (nonNull(ResponseUtils.getResponseCode())) {
+            response.setStatusCode(ResponseUtils.getResponseCode());
+        }
+        return body;
+    }
+}
