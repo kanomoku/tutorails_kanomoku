070fb7e470d82a73873bc708eb18d30638a60992
zhangziwa <1049505015@qq.com>
2024-01-12 23:14:12 +0800

    【SpringMVC】SpringMVC ResponseEntity常见使用场景 https://blog.csdn.net/weixin_37646636/article/details/135563392
    Took 42 minutes

--- practisesvr/src/main/java/com/zhangziwa/practisesvr/controller/SearchController.java
+++ practisesvr/src/main/java/com/zhangziwa/practisesvr/controller/SearchController.java
@@ -4,6 +4,9 @@ import com.zhangziwa.practisesvr.model.Student;
 import com.zhangziwa.practisesvr.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +38,18 @@ public class SearchController {
         List<Student> students = userService.listStudents3(1, 5);
         students.forEach(System.out::println);
         return students;
+    }
+
+    @RequestMapping(value = "/getAllStudents4", method = RequestMethod.GET)
+    public ResponseEntity<List<Student>> getAllStudents4() {
+        if (true){
+            throw new UnsupportedOperationException();
+        }
+
+        List<Student> students = userService.listStudents3(1, 5);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("test", "test");
+        return ResponseEntity.ok().headers(httpHeaders).contentType(MediaType.APPLICATION_JSON).body(students);
     }
 }
