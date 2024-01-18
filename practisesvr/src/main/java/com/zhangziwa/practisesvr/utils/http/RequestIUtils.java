97f259cea661eed78bbf4c0feaeb5c3aba1dac46
zhangziwa <1049505015@qq.com>
2024-01-18 23:05:47 +0800

    【网络编程】HttpServletRequest 常见方法理解 https://blog.csdn.net/weixin_37646636/article/details/134820116
    Took 2 hours 18 minutes

--- practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/http/RequestIUtils.java
+++ practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/http/RequestIUtils.java
@@ -6,6 +6,7 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,5 +29,100 @@ public class RequestIUtils {
             return new HashMap<>();
         }
         return JsonUtils.jsonToT(requestBody.toString(), Map.class);
+    }
+
+    private static HttpServletRequest getHttpServletRequest() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return null;
+        }
+        return ((ServletRequestAttributes) requestAttributes).getRequest();
+    }
+
+    public static void printRequestInfo() {
+        HttpServletRequest request = getHttpServletRequest();
+        if (request == null) {
+            return;
+        }
+        String method = request.getMethod();
+
+        String url = request.getRequestURL().toString();
+
+        String scheme = request.getScheme();
+
+        String remoteHost = request.getRemoteHost();
+        String serverName = request.getServerName();
+        String localName = request.getLocalName();
+
+        String remoteAddr = request.getRemoteAddr();
+        String localAddr = request.getLocalAddr();
+
+        int remotePort = request.getRemotePort();
+        int serverPort = request.getServerPort();
+        int localPort = request.getLocalPort();
+
+        String contextPath = request.getContextPath();
+        String requestURI = request.getRequestURI();
+        String servletPath = request.getServletPath();
+        String pathInfo = request.getPathInfo();
+
+        String queryString = request.getQueryString();
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        Enumeration<String> parameterNames = request.getParameterNames();
+
+        String contentType = request.getContentType();
+
+        int contentLength = request.getContentLength();
+
+        Enumeration<String> encodings = request.getHeaders("Accept-Encoding");
+        while (encodings.hasMoreElements()) {
+            String encoding = encodings.nextElement();
+        }
+        Enumeration<String> headerNames = request.getHeaderNames();
+        String header = request.getHeader("User-Agent");
+        int contentLength1 = request.getIntHeader("Content-Length");
+        long date = request.getDateHeader("Date");
+
+//        HttpServletMapping httpServletMapping = request.getHttpServletMapping();
+//        String authType = request.getAuthType();
+//        Cookie[] cookies = request.getCookies();
+//        String pathTranslated = request.getPathTranslated();
+//        String remoteUser = request.getRemoteUser();
+//        boolean isUserInRole = request.isUserInRole();
+//        Principal userPrincipal = request.getUserPrincipal();
+//        String requestedSessionId = request.getRequestedSessionId();
+//        HttpSession session = request.getSession();
+//        boolean isRequestedSessionIdValid = request.isRequestedSessionIdValid();
+//        boolean isRequestedSessionIdFromCookie = request.isRequestedSessionIdFromCookie();
+//        boolean isRequestedSessionIdFromURL = request.isRequestedSessionIdFromURL();
+//        Collection<Part> parts = request.getParts();
+//        Part part = request.getPart();
+//        Map<String, String> trailerFields = request.getTrailerFields();
+//        boolean isTrailerFieldsReady = request.isTrailerFieldsReady();
+
+        System.out.println(parameterMap.toString());
+        System.out.println(url);
+        System.out.println(method);
+        System.out.println(requestURI);
+        System.out.println(remoteAddr);
+        System.out.println(localAddr);
+        System.out.println(scheme);
+        System.out.println(serverName);
+        System.out.println(serverPort);
+        System.out.println(contextPath);
+        System.out.println(servletPath);
+        System.out.println(queryString);
+        System.out.println(remoteHost);
+        System.out.println(parameterNames);
+        System.out.println(header);
+        System.out.println(contentLength);
+        System.out.println(localPort);
+        System.out.println(remotePort);
+        System.out.println(localName);
+        System.out.println(contentType);
+        System.out.println(contentLength1);
+        System.out.println(date);
+        System.out.println(headerNames);
+        System.out.println(pathInfo);
     }
 }
