b003b159e2c16c2f97a7e6e647fff5cad9724a0b
zhangziwa <1049505015@qq.com>
2024-01-24 20:24:35 +0800

    【过滤器】【AntPathMatcher】过滤指定API
    Took 13 minutes

--- practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/filter/RequestCheckFilter.java
+++ practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/filter/RequestCheckFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
@@ -13,15 +14,23 @@ import java.io.IOException;
 //@Component 注册时会new 这里无需指定 registration.setFilter(new RequestCheckFilter());
 public class RequestCheckFilter extends GenericFilterBean {
 
+    public static final AntPathMatcher ANT_PATH_MATCHER = new AntPathMatcher();
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.err.println("***RequestHeaderCheckFilter.doFilter.start***");
-
-        if (!(request instanceof HttpServletRequest)) {
-            throw new IllegalStateException("Expected HttpServletRequest");
+        if (!(request instanceof HttpServletRequest httpRequest)) {
+            chain.doFilter(request, response);
+            return;
         }
 
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        // 匹配上post /api/v1/user/login 则直接放行
+        if ("POST".equalsIgnoreCase(httpRequest.getMethod())
+                && ANT_PATH_MATCHER.match("/api/v1/user/login", httpRequest.getRequestURI())) {
+            chain.doFilter(request, response);
+            return;
+        }
+
         FilterHttpServletRequest filterHttpRequest = new FilterHttpServletRequest(httpRequest);
         String body = filterHttpRequest.getBody();
         filterHttpRequest.setAttribute("requestBodyData", body);
