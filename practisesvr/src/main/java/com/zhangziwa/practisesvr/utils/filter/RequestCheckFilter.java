a80ceff1b45651b7ea826dfc3aac8dcb40ddf3d2
zhangziwa <1049505015@qq.com>
2024-01-31 00:08:25 +0800

    【SpringBoot迭代】【复习】SpringBoot中整合MyBatis SpringBoot中整合MyBatis: https://blog.csdn.net/weixin_37646636/article/details/135787361 SpringBoot初次配置数据库并初步引入业务增删改查 : https://blog.csdn.net/weixin_37646636/article/details/135428566
    Took 1 hour 32 minutes

--- practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/filter/RequestCheckFilter.java
+++ practisesvr/src/main/java/com/zhangziwa/practisesvr/utils/filter/RequestCheckFilter.java
@@ -19,16 +19,33 @@ public class RequestCheckFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.err.println("***RequestHeaderCheckFilter.doFilter.start***");
-        if (!(request instanceof HttpServletRequest httpRequest)) {
+        if (!(request instanceof HttpServletRequest)) {
             chain.doFilter(request, response);
             return;
         }
 
-        // 匹配上post /api/v1/user/login 则直接放行
-        if ("POST".equalsIgnoreCase(httpRequest.getMethod())
-                && ANT_PATH_MATCHER.match("/api/v1/user/login", httpRequest.getRequestURI())) {
-            chain.doFilter(request, response);
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        // 匹配上post /api/v1/user/login 则直接放行,主要体会ANT_PATH_MATCHER
+        if ("POST".equalsIgnoreCase(httpRequest.getMethod()) && ANT_PATH_MATCHER.match("/api/v1/user/login", httpRequest.getRequestURI())) {
+            chain.doFilter(httpRequest, response);
             return;
+        }
+
+        String contentType = httpRequest.getHeader("Content-Type");
+        if (contentType != null && contentType.contains("application/x-www-form-urlencoded")) {
+            // 请求数据是表单提交格式
+            // 这里要放行,不然原生的HttpServletRequest对象中的输入流(getInputStream())只能被读取一次，会被FilterHttpRequestBodyWrapper消耗,从而Controller层读取不到数据
+            chain.doFilter(httpRequest, response);
+            return;
+        } else if (contentType != null && contentType.contains("application/json")) {
+            // 请求数据是 JSON 格式
+        } else if (contentType != null && contentType.contains("text/plain")) {
+            // 请求数据是纯文本格式
+        } else if (contentType != null && contentType.contains("application/xml")) {
+            // 请求数据是 XML 格式
+        } else {
+            // 请求数据格式未知
         }
 
         FilterHttpRequestBodyWrapper filterHttpRequest = new FilterHttpRequestBodyWrapper(httpRequest);
