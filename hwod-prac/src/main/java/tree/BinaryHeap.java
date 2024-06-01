6e1c7dbb789d234682f0dcbc3d1a4b9311df1d44
zhangziwa <1049505015@qq.com>
2024-06-01 12:06:07 +0800

    【算法题】如何构建最小堆？ https://blog.csdn.net/weixin_37646636/article/details/139362783
    Took 11 hours 17 minutes
    
    Took 6 seconds

--- hwod-prac/src/main/java/tree/BinaryHeap.java
+++ hwod-prac/src/main/java/tree/BinaryHeap.java
@@ -4,22 +4,34 @@ import java.util.Arrays;
 
 public class BinaryHeap {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 2, 6, 5, 7, 8, 9, 10, 0};
+        int[] arr = {1, 3, 2, 9, 5, 7, 8, 6, 10, 0};
         System.out.println("原始数据：" + Arrays.toString(arr));
-
-
-        upAdjust(arr);
-        System.out.println("上浮构建二叉堆：" + Arrays.toString(arr));
-
-        int[] arr1 = {1, 3, 2, 6, 5, 7, 8, 9, 10, 0};
-        for (int i = (arr1.length - 1) / 2; i >= 0; i--) {
-            downAdjust(arr1, i, arr.length);
+        for (int i = arr.length - 1; i >= 0; i--) {
+            smallUp1(arr, i);
         }
-        System.out.println("下沉构建二叉堆：" + Arrays.toString(arr1));
+        System.out.println("上浮构建最小二叉堆：" + Arrays.toString(arr));
 
-        int[] arr2 = {1, 3, 2, 6, 5, 7, 8, 9, 10, 0};
-        heapSort(arr2);
-        System.out.println("二叉堆排序：" + Arrays.toString(arr2));
+        int[] arr2 = {1, 3, 2, 9, 5, 7, 8, 6, 10, 0};
+        for (int i = arr2.length - 1; i >= 0; i--) {
+            smallUp2(arr2, i);
+        }
+        System.out.println("上浮构建最小二叉堆：" + Arrays.toString(arr2));
+
+        int[] arr11 = {1, 3, 2, 9, 5, 7, 8, 6, 10, 0};
+        for (int i = (arr11.length - 1) / 2; i >= 0; i--) {
+            bigDown1(arr11, i, arr11.length);
+        }
+        System.out.println("下沉构建最小二叉堆：" + Arrays.toString(arr11));
+
+        int[] arr22 = {1, 3, 2, 9, 5, 7, 8, 6, 10, 0};
+        for (int i = (arr22.length - 1) / 2; i >= 0; i--) {
+            bigDown2(arr22, i, arr22.length);
+        }
+        System.out.println("下沉构建最小二叉堆：" + Arrays.toString(arr22));
+
+        //        int[] arr22 = {1, 3, 2, 9, 5, 7, 8, 6, 10, 0};
+        //        heapSort(arr22);
+        //        System.out.println("二叉堆排序：" + Arrays.toString(arr22));
     }
 
     /**
@@ -27,61 +39,96 @@ public class BinaryHeap {
      */
     public static void heapSort(int[] arr) {
         // 无序数组构造成二叉堆
-        upAdjust(arr);
-        System.out.println("上浮构建二叉堆：" + Arrays.toString(arr));
+        //        smallUp2(arr);
+        //        System.out.println("上浮构建二叉堆：" + Arrays.toString(arr));
 
         for (int i = arr.length - 1; i >= 0; i--) {
 
-            // 把小的置换到最下面
             int temp = arr[i];
             arr[i] = arr[0];
             arr[0] = temp;
 
             // 把i作为右边界的范围内最小的找出来
-            downAdjust(arr, 0, i);
+            //            downAdjust(arr, 0, i);
+            bigDown2(arr, 0, i);
         }
     }
 
     /**
-     * 下浮调整(大的沉下去)
+     * 下浮调整(大的下沉)
      *
      * @param arr    待调整的堆
      * @param parent 要下沉的父节点
      * @param length 堆的有效大小
      */
-    public static void downAdjust(int[] arr, int parent, int length) {
-        int deseNote = arr[parent]; // 根节点
-        int child = 2 * parent + 1;// 求左子节点
-
+    public static void bigDown1(int[] arr, int parent, int length) {
+        int child = 2 * parent + 1;
         while (child < length) { // 范围内
             if (child + 1 < length && arr[child + 1] < arr[child]) { // 取出两个子节点值最小的那个
                 child++;
             }
-            if (deseNote <= arr[child]) { // 父节点比他们都小，则不需要挪位置，终止循环
+            if (arr[parent] <= arr[child]) {       // 父节点比他们都小，则符合预期终止循环
                 break;
             }
-            arr[parent] = arr[child]; // 子节点小，则子节点位置上移
-            parent = child; // 此时子节点视为父节点继续下一步处理
-            child = 2 * child + 1; // 算子节点的子节点
+            arr[child] = arr[child] ^ arr[parent];
+            arr[parent] = arr[child] ^ arr[parent];
+            arr[child] = arr[child] ^ arr[parent];
+            parent = child;                         // 此时子节点视为父节点继续下一步处理
+            child = 2 * child + 1;
         }
-
-        arr[parent] = deseNote;// 嘚瑟不动了，钻进别人剩下的窝里
     }
 
     /**
-     * 上浮调整(小的浮上来)
+     * 下浮调整(大的下沉)
+     *
+     * @param arr    待调整的堆
+     * @param parent 要下沉的父节点
+     * @param length 堆的有效大小
      */
-    public static void upAdjust(int[] arr) {
-        int child = arr.length - 1;// 最后一个节点的下标
-        int parent = (child - 1) / 2;// 最后一个非叶子节点的下标
-        int deseNote = arr[child]; // 往下挪，所以把下面的节点取出来（嘚瑟一圈去，实在不行再回这个位置）
+    public static void bigDown2(int[] arr, int parent, int length) {
+        int baseVal = arr[parent];
+        int child = 2 * parent + 1;
+        while (child < length) {
+            if (child + 1 < length && arr[child + 1] < arr[child]) {
+                child++;
+            }
+            if (baseVal <= arr[child]) {
+                break;
+            }
+            arr[parent] = arr[child]; // 子节点小，则子节点位置上移
+            parent = child;
+            child = 2 * child + 1;
+        }
+        arr[parent] = baseVal;        // baseVal下沉不动了，所以落在当前子节点位置
+    }
 
-        while (0 < child && deseNote < arr[parent]) { // 说明这个节点还是叶子节点还不是根节点、父节点值>字节点的值
-            arr[child] = arr[parent]; // 父节点值挪下来
-            child = parent; // 父节点此时开始视为子节点
-            parent = (child - 1) / 2; // 算父节点的父节点
+    /**
+     * 上浮调整(小的上浮)
+     */
+    public static void smallUp1(int[] arr, int child) {
+        int parent = (child - 1) / 2;
+
+        while (0 < child && arr[child] < arr[parent]) { // 0 < child说明这个节点还是叶子
+            arr[child] = arr[child] ^ arr[parent];
+            arr[parent] = arr[child] ^ arr[parent];
+            arr[child] = arr[child] ^ arr[parent];
+            child = parent;                             // 父节点此时开始视为子节点
+            parent = (child - 1) / 2;                   // 算父节点的父节点
+        }
+    }
+
+    /**
+     * 上浮调整(小的上浮)
+     */
+    public static void smallUp2(int[] arr, int child) {
+        int parent = (child - 1) / 2;
+        int baseVal = arr[child];                       // 把处理的数据取出来
+        while (0 < child && baseVal < arr[parent]) {
+            arr[child] = arr[parent];                   // 父节点值挪下来，父节点为baseVal备选位置
+            child = parent;
+            parent = (child - 1) / 2;
         }
 
-        arr[child] = deseNote; // 当前节点>父节点，因为当前节点的值已经赋值给他的子节点了，所有其位置装出去嘚瑟的值
+        arr[child] = baseVal;                           // baseVal上浮不动了，所以落在当前子节点位置
     }
 }
